11e3eb0a73afc3c938e28c6fec8bcb7683beddcd
Ronak Lakhotia <ronak.lakhotia999@gmail.com>
2019-01-25 16:57:06 +0800

    User Authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { SharedModule } from './shared/shared.module';
 import { HeaderComponent } from './shared/layout';
 import {IssueModule} from './issue/issue.module';
 import {AuthModule} from './auth/auth.module';
+import { HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -22,6 +23,7 @@ import {AuthModule} from './auth/auth.module';
     AuthModule,
     SharedModule,
     AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders} from "@angular/common/http";
 import {Router} from '@angular/router';
 import {BehaviorSubject} from 'rxjs';
 import {NgZone} from '@angular/core';
 import {ElectronService} from './electron.service';
+import {ErrorHandlingService} from '../../core/services/error-handling.service';
+import {GithubService} from "../../core/services/github.service";
 
 export enum AuthState { 'NotAuthenticated', 'AwaitingAuthentication', 'Authenticated' }
 
@@ -14,7 +17,9 @@ export class AuthService {
   authStateSource = new BehaviorSubject(AuthState.NotAuthenticated);
   currentAuthState = this.authStateSource.asObservable();
 
-  constructor(private electronService: ElectronService, private router: Router, private ngZone: NgZone) {
+  constructor(private electronService: ElectronService, private router: Router, private ngZone: NgZone,
+              private http: HttpClient,  private errorHandlingService: ErrorHandlingService,
+              private githubService: GithubService) {
     // When OAuth process is completed ipcMain from electron will send the access_token through this channel.
     this.electronService.ipcRenderer.on('github-oauth-reply', (event, access_token) => {
       this.changeAuthState(AuthState.Authenticated);
@@ -28,7 +33,22 @@ export class AuthService {
   }
 
   startAuthentication(username: String, password: String) {
-
+    this.changeAuthState(AuthState.AwaitingAuthentication);
+    let header = new HttpHeaders().set("Authorization", "Basic "+ btoa(username + ':' + password));
+    this.http.get('https://api.github.com/user', { headers: header })
+      .subscribe(
+        response => {
+          console.log(response);
+          this.changeAuthState(AuthState.Authenticated);
+          console.log(this.isAuthenticated());
+          this.githubService.storeCredentials(username, password);
+          this.ngZone.run(() => this.router.navigate(['']));
+        },
+        error => {
+          console.log(error);
+          this.errorHandlingService.handleHttpError(error);
+        }
+      );
   }
 
   logOut(): void {
@@ -38,7 +58,8 @@ export class AuthService {
   }
 
   isAuthenticated(): boolean {
-    return !!this.accessToken;
+    console.log(this.authStateSource.getValue().toString());
+    return !!(this.authStateSource.getValue() == 2);
   }
 
   private initializeLoggedInUser(access_token: string): void {
