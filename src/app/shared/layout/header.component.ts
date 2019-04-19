1606c70c606b3cfb5f2665d4b296539d3eb33421
Koh Jun Wei <kjunwei1996@gmail.com>
2019-04-19 14:29:09 +0800

    Edit back button

--- src/app/shared/layout/header.component.ts
+++ src/app/shared/layout/header.component.ts
@@ -3,15 +3,25 @@ import {Location} from '@angular/common';
 import {AuthService} from '../../core/services/auth.service';
 import {PhaseService} from '../../core/services/phase.service';
 import {UserService} from '../../core/services/user.service';
-import {Router} from '@angular/router';
+import {NavigationEnd, Router, RoutesRecognized} from '@angular/router';
+import {filter, pairwise} from 'rxjs/operators';
 
 @Component({
   selector: 'app-layout-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent implements OnInit {
+  private prevUrl;
+
   constructor(private router: Router, public auth: AuthService, public phaseService: PhaseService, public userService: UserService,
-              private location: Location) {}
+              private location: Location) {
+    router.events.pipe(
+      filter((e: any) => e instanceof RoutesRecognized),
+      pairwise()
+    ).subscribe(e => {
+      this.prevUrl = e[0].urlAfterRedirects;
+    });
+  }
 
   ngOnInit() {}
 
@@ -20,7 +30,11 @@ export class HeaderComponent implements OnInit {
   }
 
   goBack() {
-    this.location.back();
+    if (this.prevUrl === `/${this.phaseService.currentPhase}/issues/new`) {
+      this.router.navigate(['/phase1']);
+    } else {
+      this.location.back();
+    }
   }
 
   logOut() {
