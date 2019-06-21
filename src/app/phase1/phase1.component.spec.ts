8834d8eb733277863216e52fe1339012c9cab24e
Kevin Chin <chin_kevin93@hotmail.com>
2019-06-21 12:05:25 +0800

    jasmine unit test

--- src/app/phase1/phase1.component.spec.ts
+++ src/app/phase1/phase1.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Phase1Component } from './home.component';
+import { Phase1Component } from './phase1.component';
 import { TranslateModule } from '@ngx-translate/core';
 
 describe('HomeComponent', () => {
