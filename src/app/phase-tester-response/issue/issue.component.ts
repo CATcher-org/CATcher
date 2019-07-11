7d57dd80dcb658197770cbd935ae0b996b94ac1a
Kevin Chin <chin_kevin93@hotmail.com>
2019-07-11 14:34:26 +0800

    setup issue component

--- src/app/phase-tester-response/issue/issue.component.ts
+++ src/app/phase-tester-response/issue/issue.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ViewIssueComponent, ISSUE_COMPONENTS } from '../../shared/view-issue/view-issue.component';
 
 @Component({
   selector: 'app-issue',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./issue.component.css']
 })
 export class IssueComponent implements OnInit {
+  issueId: number;
 
-  constructor() { }
+  readonly issueComponents: ISSUE_COMPONENTS[] = [
+    ISSUE_COMPONENTS.TESTER_POST,
+    ISSUE_COMPONENTS.SEVERITY_LABEL,
+    ISSUE_COMPONENTS.TYPE_LABEL,
+    ISSUE_COMPONENTS.TESTER_POST,
+    ISSUE_COMPONENTS.TEAM_RESPONSE,
+    ISSUE_COMPONENTS.NEW_TESTER_RESPONSE,
+    ISSUE_COMPONENTS.TESTER_RESPONSE,
+    ISSUE_COMPONENTS.SEVERITY_LABEL,
+    ISSUE_COMPONENTS.TYPE_LABEL,
+    ISSUE_COMPONENTS.RESPONSE_LABEL,
+    ISSUE_COMPONENTS.ASSIGNEE,
+    ISSUE_COMPONENTS.DUPLICATE
+  ];
+
+  @ViewChild(ViewIssueComponent) viewIssue: ViewIssueComponent;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.subscribe(
+      params => {
+        this.issueId = + params['issue_id'];
+      }
+    );
+  }
+
+  canDeactivate(): boolean {
+    return this.viewIssue.isEditing();
   }
 
 }
