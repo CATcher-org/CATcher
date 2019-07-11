c9190984a4b530645808e5012a85bca1e68056ef
ptvrajsk <ptvrajsk@gmail.com>
2019-07-11 17:08:45 +0800

    Search Filter

--- src/app/phase-tester-response/issue-pending/issue-pending.component.ts
+++ src/app/phase-tester-response/issue-pending/issue-pending.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import {ACTION_BUTTONS} from '../../shared/issue-tables/issue-tables.component';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {ACTION_BUTTONS, IssueTablesComponent} from '../../shared/issue-tables/issue-tables.component';
+import {Issue} from '../../core/models/issue.model';
 
 @Component({
   selector: 'app-issue-pending',
@@ -8,13 +9,20 @@ import {ACTION_BUTTONS} from '../../shared/issue-tables/issue-tables.component';
 })
 export class IssuePendingComponent implements OnInit {
 
+  @ViewChild(IssueTablesComponent) table: IssueTablesComponent;
+
   readonly displayedColumns: string[] = ['id', 'title', 'type', 'severity', 'actions'];
   readonly actionButtons: ACTION_BUTTONS[] = [ACTION_BUTTONS.VIEW_IN_WEB, ACTION_BUTTONS.RESPOND_TO_ISSUE,
     ACTION_BUTTONS.MARK_AS_RESPONDED];
+  readonly filter: (issue: Issue) => boolean = (issue: Issue) => (issue.teamResponse !== undefined && issue.testerResponses === undefined);
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  applyFilter(filterValue: string) {
+    this.table.issues.filter = filterValue;
   }
 
 }
--- src/app/phase-tester-response/issue-responded/issue-responded.component.ts
+++ src/app/phase-tester-response/issue-responded/issue-responded.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import {ACTION_BUTTONS} from '../../shared/issue-tables/issue-tables.component';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {ACTION_BUTTONS, IssueTablesComponent} from '../../shared/issue-tables/issue-tables.component';
+import {Issue} from '../../core/models/issue.model';
 
 @Component({
   selector: 'app-issue-responded',
@@ -8,11 +9,19 @@ import {ACTION_BUTTONS} from '../../shared/issue-tables/issue-tables.component';
 })
 export class IssueRespondedComponent implements OnInit {
 
+  @ViewChild(IssueTablesComponent) table: IssueTablesComponent;
+
   readonly displayedColumns: string[] = ['id', 'title', 'type', 'severity', 'actions'];
   readonly actionButtons: ACTION_BUTTONS[] = [ACTION_BUTTONS.VIEW_IN_WEB, ACTION_BUTTONS.MARK_AS_PENDING];
+  readonly filter: (issue: Issue) => boolean = (issue: Issue) => (issue.teamResponse !== undefined && issue.testerResponses !== undefined);
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  applyFilter(filterValue: string) {
+    this.table.issues.filter = filterValue;
   }
 
 }
