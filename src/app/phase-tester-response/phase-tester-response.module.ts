00f2da7f9b87ee95eb5672a4440ddbe95938865a
Kevin Chin <chin_kevin93@hotmail.com>
2019-07-11 14:42:13 +0800

    add missing imports

--- src/app/phase-tester-response/phase-tester-response.module.ts
+++ src/app/phase-tester-response/phase-tester-response.module.ts
@@ -4,13 +4,21 @@ import { CommonModule } from '@angular/common';
 import { PhaseTesterResponseRoutingModule } from './phase-tester-response-routing.module';
 import { PhaseTesterResponseComponent } from './phase-tester-response.component';
 import { IssueComponent } from './issue/issue.component';
+import { ViewIssueModule } from '../shared/view-issue/view-issue.module';
+import { IssueTablesModule } from '../shared/issue-tables/issue-tables.module';
+import { MarkdownModule } from 'ngx-markdown';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   exports: [PhaseTesterResponseComponent],
   declarations: [PhaseTesterResponseComponent, IssueComponent],
   imports: [
     CommonModule,
-    PhaseTesterResponseRoutingModule
+    PhaseTesterResponseRoutingModule,
+    SharedModule,
+    ViewIssueModule,
+    IssueTablesModule,
+    MarkdownModule.forChild()
   ]
 })
 export class PhaseTesterResponseModule { }
