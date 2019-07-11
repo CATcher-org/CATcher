26c7efc6382e2daeca16cb9d2ceb7a718ae76df1
Kevin Chin <chin_kevin93@hotmail.com>
2019-07-11 12:22:52 +0800

    add skeleton files for issue component

---
+++ src/app/phase-tester-response/issue/issue.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-issue',
+  templateUrl: './issue.component.html',
+  styleUrls: ['./issue.component.css']
+})
+export class IssueComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/phase-tester-response/phase-tester-response.module.ts
+++ src/app/phase-tester-response/phase-tester-response.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { PhaseTesterResponseRoutingModule } from './phase-tester-response-routing.module';
 import { PhaseTesterResponseComponent } from './phase-tester-response.component';
+import { IssueComponent } from './issue/issue.component';
 
 @NgModule({
   exports: [PhaseTesterResponseComponent],
-  declarations: [PhaseTesterResponseComponent],
+  declarations: [PhaseTesterResponseComponent, IssueComponent],
   imports: [
     CommonModule,
     PhaseTesterResponseRoutingModule
