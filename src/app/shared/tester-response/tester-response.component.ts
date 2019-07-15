ed554a7bdd95ca6488c9243d531612cfc6c01cfb
Kevin Chin <chin_kevin93@hotmail.com>
2019-07-15 11:09:07 +0800

    set up tester-response component

---
+++ src/app/shared/tester-response/tester-response.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tester-response',
+  templateUrl: './tester-response.component.html',
+  styleUrls: ['./tester-response.component.css']
+})
+export class TesterResponseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
