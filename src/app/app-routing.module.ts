a5d0a3f16fe670781109fea71e3e8e101a9bfdd3
Kevin Chin <chin_kevin93@hotmail.com>
2019-07-17 11:28:54 +0800

    add missing import space

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { AuthModule} from './auth/auth.module';
-import {Phase1Module} from './phase1/phase1.module';
-import {Phase2Module} from './phase2/phase2.module';
-import {Phase3Module} from './phase3/phase3.module';
-import {AuthGuard} from './core/guards/auth.guard';
+import { Phase1Module } from './phase1/phase1.module';
+import { Phase2Module } from './phase2/phase2.module';
+import { Phase3Module } from './phase3/phase3.module';
+import { AuthGuard } from './core/guards/auth.guard';
 import { PhaseTesterResponseModule } from './phase-tester-response/phase-tester-response.module';
 
 const routes: Routes = [
