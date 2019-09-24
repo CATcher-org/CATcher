b61002f61279c7f1a08b8f7eca62fc93f1431a10
Prithivi Raj <ptvrajsk@gmail.com>
2019-09-24 18:32:34 +0800

    Lint Fix

--- src/app/core/models/github-issue.model.ts
+++ src/app/core/models/github-issue.model.ts
@@ -1,3 +1,51 @@
+export class GithubLabel {
+  static readonly LABEL_ORDER = {
+    severity: { Low: 0, Medium: 1, High: 2 },
+    type: { DocumentationBug: 0, FunctionalityBug: 1 },
+  };
+  static readonly LABELS = {
+    severity: 'severity',
+    type: 'type',
+    response: 'response',
+    duplicated: 'duplicated',
+    status: 'status',
+    unsure: 'unsure',
+    pending: 'pending',
+    team: 'team',
+    tutorial: 'tutorial'
+  };
+
+  color: string;
+  id: number;
+  name: string;
+  url: string;
+
+  constructor(githubLabels: {}) {
+    Object.assign(this, githubLabels);
+  }
+
+  getCategory(): string {
+    if (this.isCategorical()) {
+      return this.name.split('.')[0];
+    } else {
+      return this.name;
+    }
+  }
+
+  getValue(): string {
+    if (this.isCategorical()) {
+      return this.name.split('.')[1];
+    } else {
+      return this.name;
+    }
+  }
+
+  isCategorical(): boolean {
+    const regex = /^\S+.\S+$/;
+    return regex.test(this.name);
+  }
+}
+
 export class GithubIssue {
   id: number; // Github's backend's id
   number: number; // Issue's display id
@@ -61,53 +109,5 @@ export class GithubIssue {
 
   findTeamId(): string {
     return `${this.findLabel('team')}.${this.findLabel('tutorial')}`;
-  }
-}
-
-export class GithubLabel {
-  static readonly LABEL_ORDER = {
-    severity: { Low: 0, Medium: 1, High: 2 },
-    type: { DocumentationBug: 0, FunctionalityBug: 1 },
-  };
-  static readonly LABELS = {
-    severity: 'severity',
-    type: 'type',
-    response: 'response',
-    duplicated: 'duplicated',
-    status: 'status',
-    unsure: 'unsure',
-    pending: 'pending',
-    team: 'team',
-    tutorial: 'tutorial'
-  };
-
-  color: string;
-  id: number;
-  name: string;
-  url: string;
-
-  constructor(githubLabels: {}) {
-    Object.assign(this, githubLabels);
-  }
-
-  getCategory(): string {
-    if (this.isCategorical()) {
-      return this.name.split('.')[0];
-    } else {
-      return this.name;
-    }
-  }
-
-  getValue(): string {
-    if (this.isCategorical()) {
-      return this.name.split('.')[1];
-    } else {
-      return this.name;
-    }
-  }
-
-  isCategorical(): boolean {
-    const regex = /^\S+.\S+$/;
-    return regex.test(this.name);
   }
 }
