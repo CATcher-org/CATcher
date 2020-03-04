9a496b17a9b231813427f1751464751182ae576f
Koh Jun Wei <kjunwei1996@gmail.com>
2020-03-04 16:29:05 +0800

    Remove redundant import

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -13,7 +13,6 @@ import { UserService } from '../core/services/user.service';
 import { GithubEventService } from '../core/services/githubevent.service';
 import { ElectronService } from '../core/services/electron.service';
 import { ApplicationService } from '../core/services/application.service';
-import { DataService } from '../core/services/data.service';
 import { session } from 'electron';
 import { GithubUser } from '../core/models/github-user.model';
 
