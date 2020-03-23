2e10f62b0f7e138da1d843df3aaa5723a9a5de55
anubh-v <anubhav@u.nus.edu>
2020-03-23 22:30:58 +0800

    Test that error is thrown on invalid filetype

--- tests/services/upload.service.spec.ts
+++ tests/services/upload.service.spec.ts
@@ -1,4 +1,4 @@
-import {UploadService, SUPPORTED_FILE_TYPES} from '../../src/app/core/services/upload.service';
+import {UploadService, SUPPORTED_FILE_TYPES, FILE_TYPE_SUPPORT_ERROR} from '../../src/app/core/services/upload.service';
 
 describe('Test the UploadService', () => {
     it('Test whether the UploadService can detect valid / invalid filetypes', () => {
@@ -9,6 +9,20 @@ describe('Test the UploadService', () => {
         const invalidFileName = "testFile." + 'java';
         expect(uploadService.isSupportedFileType(validFileName)).toBe(true);
         expect(uploadService.isSupportedFileType(invalidFileName)).toBe(false);
+    });
+
+    it('UploadService should throw an error if an invalid filetype is uploaded', done => {
+      const uploadService = new UploadService(null);
+      uploadService.uploadFile('testdata', 'testFile.java').subscribe(
+        val => {
+          fail('This test case should fail.');
+          done();
+        },
+        error => {
+          expect(error).toBe(FILE_TYPE_SUPPORT_ERROR);
+          done();
+        }
+      );
     });
 });
 
