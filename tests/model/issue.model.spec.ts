788adc2f460d6decae3fb44d43f9737fa53a1968
anubh-v <anubhav@u.nus.edu>
2020-04-03 18:11:36 +0800

    Reorganise spec

--- tests/model/issue.model.spec.ts
+++ tests/model/issue.model.spec.ts
@@ -3,17 +3,19 @@ import { Issue } from '../../src/app/core/models/issue.model'
 describe('Issue model class', () => {
     describe('.updateDescription(description) and .updateTeamResponse(teamResponse)', () => {
         it('correctly clean strings obtained from users', () => {
-            expect(Issue.updateDescription('')).toBe('No details provided by bug reporter.');
-            expect(Issue.updateTeamResponse('')).toBe('No details provided by team.');
-            expect(Issue.updateDescription(null)).toBe('No details provided by bug reporter.');
-            expect(Issue.updateTeamResponse(undefined)).toBe('No details provided by team.');
+            const noDetailsFromBugReporter = 'No details provided by bug reporter.';
+            const noDetailsFromTeam = 'No details provided by team.';
+            expect(Issue.updateDescription('')).toBe(noDetailsFromBugReporter);
+            expect(Issue.updateTeamResponse('')).toBe(noDetailsFromTeam);
+            expect(Issue.updateDescription(null)).toBe(noDetailsFromBugReporter);
+            expect(Issue.updateTeamResponse(undefined)).toBe(noDetailsFromTeam);
 
-            const typicalDescription = 'The app crashes after parsing config file.s'
+            const typicalDescription = 'The app crashes after parsing config files.'
             const typicalTeamResponse = 'Cannot replicate the bug.'
             expect(Issue.updateDescription(typicalDescription)).toBe(typicalDescription);
             expect(Issue.updateTeamResponse(typicalTeamResponse)).toBe(typicalTeamResponse);
 
-            const inputWithSpecialChars = '$%^!@&&_0_99';
+            const inputWithSpecialChars = '$%^!@&-_test';
             expect(Issue.updateDescription(inputWithSpecialChars)).toBe(inputWithSpecialChars);
             expect(Issue.updateTeamResponse(inputWithSpecialChars)).toBe(inputWithSpecialChars);
         });
