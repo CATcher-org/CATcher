43dccc0771dfa6983baafe32f0917858b005dd67
anubh-v <anubhav@u.nus.edu>
2020-04-03 17:36:29 +0800

    Improve organisation of test specs

--- tests/model/issue.model.spec.ts
+++ tests/model/issue.model.spec.ts
@@ -1,21 +1,26 @@
 import { Issue } from '../../src/app/core/models/issue.model'
 import { ISSUE_WITH_EMPTY_DESCRIPTION, ISSUE_WITH_ASSIGNEES } from '../constants/githubissue.constants'
-import { TEAM_RESPONSE } from '../constants/githubcomment.constants';
+import { EMPTY_TEAM_RESPONSE } from '../constants/githubcomment.constants';
 
-describe("Test whether issue instances are correctly created from GitHubIssue instances", () => {
-    it('Create a bug reporting issue with an empty description', async () => {
-        const issue = Issue.createPhaseBugReportingIssue(ISSUE_WITH_EMPTY_DESCRIPTION);
-        expect(issue.title).toBe('App starts to lag when given large amount of input');
-        expect(issue.description).toBe('No details provided by bug reporter.');
-        expect(issue.severity).toBe('Medium');
-        expect(issue.type).toBe('FunctionalityBug');
+describe('Issue model class', () => {
+    describe('.createPhaseBugReportIssue(githubIssue)', () => {
+        it('correctly creates a bug reporting issue that has an empty description', async () => {
+            const issue = Issue.createPhaseBugReportingIssue(ISSUE_WITH_EMPTY_DESCRIPTION);
+            expect(issue.title).toBe('App starts to lag when given large amount of input');
+            expect(issue.description).toBe('No details provided by bug reporter.');
+            expect(issue.severity).toBe('Medium');
+            expect(issue.type).toBe('FunctionalityBug');
+        });
     });
-
-    it('Create a team response issue that has an empty team response', async() => {
-        const dummyTeam = null;
-        const issue = Issue.createPhaseTeamResponseIssue(ISSUE_WITH_ASSIGNEES, [TEAM_RESPONSE], dummyTeam);
-        expect(issue.title).toBe('Screen freezes');
-        expect(issue.teamResponse).toBe('No details provided by team.');
-        expect(issue.assignees).toContain('anubh-v');
-    })
+    describe('.createPhaseTeamResponseIssue(githubIssue, githubComment)', () => {
+        it('correctly creates a team response issue that has an empty team response', async() => {
+            const dummyTeam = null;
+            const issue = Issue.createPhaseTeamResponseIssue(ISSUE_WITH_ASSIGNEES, [EMPTY_TEAM_RESPONSE], dummyTeam);
+            expect(issue.title).toBe('Screen freezes');
+            expect(issue.teamResponse).toBe('No details provided by team.');
+            expect(issue.severity).toBe('Medium');
+            expect(issue.assignees).toContain('anubh-v');
+            expect(issue.assignees.length).toBe(1);
+        });
+    });
 });
