8e5ba988aa78b28bcc07bd92cb066ec1041f2c92
anubh-v <anubhav@u.nus.edu>
2020-04-15 01:30:12 +0800

    Improve code style, test for case insensitivity and test correctness with short loginIds

--- tests/services/user.service.spec.ts
+++ tests/services/user.service.spec.ts
@@ -1,42 +1,47 @@
-import { jsonData, USER_JUNWEI, USER_ANUBHAV, USER_SHUMING } from '../constants/data.constant'
+import { jsonData, USER_JUNWEI, USER_Q, USER_SHUMING } from '../constants/data.constants'
 import { UserService } from '../../src/app/core/services/user.service'
+import { User, UserRole } from '../../src/app/core/models/user.model';
 import { of } from 'rxjs';
-import { User } from '../../src/app/core/models/user.model';
-import { doesNotReject } from 'assert';
 
 let dataService: any;
 
-describe('UserService should correctly create User objects', () => {
-  beforeAll(() => {
-    dataService = jasmine.createSpyObj("DataService", ['getDataFile']);
-    dataService.getDataFile.and.returnValue(of(jsonData));
-  });
+describe('UserService', () => {
+  describe('.createUserModel(loginId)', () => {
+    beforeAll(() => {
+      dataService = jasmine.createSpyObj("DataService", ['getDataFile']);
+      dataService.getDataFile.and.returnValue(of(jsonData));
+    });
 
-  it('should create a Student user correctly', async () => {
-    await createAndVerifyUser(USER_JUNWEI.loginId, USER_JUNWEI);
-  });
+    it('creates a Student user correctly', async () => {
+      await createAndVerifyUser(USER_JUNWEI.loginId, USER_JUNWEI);
+    });
 
-  it('should create a Tutor user correctly', async () => {
-    await createAndVerifyUser(USER_ANUBHAV.loginId, USER_ANUBHAV);
-  });
+    it('creates a Tutor user correctly when loginId is very short', async () => {
+      await createAndVerifyUser(USER_Q.loginId, USER_Q);
+    });
 
-  it('should create an Admin user correctly', async () => {
-    await createAndVerifyUser(USER_SHUMING.loginId, USER_SHUMING);
-  });
+    it('creates an Admin user correctly', async () => {
+      await createAndVerifyUser(USER_SHUMING.loginId, USER_SHUMING);
+    });
 
-  it('should throw an error if the user is unauthorized', (done) => {
-    const userService = new UserService(null, dataService);
-    userService.createUserModel('RandomUser').subscribe(
-      user => {
-        fail('This test case should have failed.');
-        done();
-      },
-      error => {
-        expect(error).toBe('Unauthorized user.');
-        done();
-      }
-    );
-  });
+    it('treats the loginId in a case insensitive manner', async () => {
+      await createAndVerifyUser('JUNWEi96', USER_JUNWEI);
+    });
+
+    it('throws an error if the user is unauthorized', (done) => {
+      const userService = new UserService(null, dataService);
+      userService.createUserModel('RandomUser').subscribe(
+        user => {
+          fail('This test case should have failed.');
+          done();
+        },
+        error => {
+          expect(error).toBe('Unauthorized user.');
+          done();
+        }
+      );
+    });
+});
 });
 
 async function createAndVerifyUser(loginId: string, expectedUser: User) {
