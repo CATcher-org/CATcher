897a3402b935338b67f1c29428e2bfdad3bdb3ab
anubh-v <anubhav@u.nus.edu>
2020-08-17 18:29:55 +0800

    Add completed tests

--- tests/app/shared/label-dropdown/label-dropdown.component.spec.ts
+++ tests/app/shared/label-dropdown/label-dropdown.component.spec.ts
@@ -1,14 +1,14 @@
-import { LabelDropdownComponent } from '../../../../src/app/shared/label-dropdown/label-dropdown.component';
+import { LabelDropdownComponent, BLACK_TEXT_CLASS, WHITE_TEXT_CLASS } from '../../../../src/app/shared/label-dropdown/label-dropdown.component';
 import { LabelService } from '../../../../src/app/core/services/label.service';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { Label } from '../../../../src/app/core/models/label.model';
+import { SEVERITY_LABELS, COLOR_WHITE, SEVERITY } from '../../../constants/label.constants';
 
 describe('LabelDropdownComponent', () => {
-
   let labelDropdownComponent: LabelDropdownComponent;
   let labelService: any;
+
   beforeEach(() => {
-    labelService = jasmine.createSpyObj(LabelService, ['getLabelList', 
+    labelService = jasmine.createSpyObj(LabelService, ['getLabelList',
       'getColorOfLabel', 'isDarkColor']);
 
     const formGroup: FormGroup = new FormBuilder().group(({
@@ -20,23 +20,24 @@ describe('LabelDropdownComponent', () => {
 
     labelDropdownComponent = new LabelDropdownComponent(labelService);
     labelDropdownComponent.dropdownForm = formGroup;
-  })
-
-  it('should initialise its label list', () => {
-    labelDropdownComponent.attributeName = 'severity';
-    labelDropdownComponent.initialValue = '';
-    const labels: Label[] = [new Label('severity', 'High', 'ff6666')];
-    labelService.getLabelList.and.returnValue(labels);
-    labelService.getColorOfLabel.and.returnValue('ff6666');
-
-    labelDropdownComponent.ngOnInit();
-    expect(labelDropdownComponent.labelList).toEqual(labels);
-    expect(labelDropdownComponent.selectedColor).toEqual('ff6666');
   });
 
-  it('should change color upon selection change event', () => {
-    
-  }
+  it('should be initialised with a list of labels, an initial colour and a dropdown control', () => {
+    labelDropdownComponent.attributeName = SEVERITY;
+    labelDropdownComponent.initialValue = '';
+    labelService.getLabelList.and.returnValue(SEVERITY_LABELS);
+    labelService.getColorOfLabel.and.returnValue(COLOR_WHITE);
 
+    labelDropdownComponent.ngOnInit();
+    expect(labelDropdownComponent.labelList).toEqual(SEVERITY_LABELS);
+    expect(labelDropdownComponent.selectedColor).toEqual(COLOR_WHITE);
+    expect(labelDropdownComponent.dropdownControl).toBeDefined();
+  });
 
+  it('should set its dropdown text colour based on the darkness of the current colour', () => {
+    labelService.isDarkColor.and.returnValue(false);
+    expect(labelDropdownComponent.dropdownTextColor).toBe(BLACK_TEXT_CLASS);
+    labelService.isDarkColor.and.returnValue(true);
+    expect(labelDropdownComponent.dropdownTextColor).toBe(WHITE_TEXT_CLASS);
+  });
 });
