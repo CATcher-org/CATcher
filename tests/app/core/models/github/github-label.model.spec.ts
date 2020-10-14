ec8e0d2d3b5ade0d0c018c6ef8de68cc7112ae22
Low Jun Kai, Sean <sean.low.jk@gmail.com>
2020-10-14 20:28:36 +0800

    Add IsCategorical Tests

---
+++ tests/app/core/models/github/github-label.model.spec.ts
@@ -0,0 +1,37 @@
+import { GithubLabel } from '../../../../../src/app/core/models/github/github-label.model';
+
+describe('GithubLabel', () => {
+  let githubLabel: GithubLabel;
+  const category = 'severity';
+  const value = 'LOW';
+
+  it('should returns false on isCategorical() when provided wit no name', () => {
+    githubLabel = new GithubLabel({ });
+    expect(githubLabel.isCategorical()).toEqual(false);
+  });
+
+  it('should returns false on isCategorical() when provided with invalid label name', () => {
+    githubLabel = new GithubLabel({ name: `${category}${value}`  });
+    expect(githubLabel.isCategorical()).toEqual(false);
+
+    githubLabel = new GithubLabel({ name: `${category}             ${value}`  });
+    expect(githubLabel.isCategorical()).toEqual(false);
+  });
+
+  it('should returns true on isCategorical() when provided with valid label name', () => {
+    githubLabel = new GithubLabel({ name: `${category}.${value}` });
+    expect(githubLabel.isCategorical()).toEqual(true);
+
+    githubLabel = new GithubLabel({ name: `${category}.......${value}` });
+    expect(githubLabel.isCategorical()).toEqual(true);
+
+    githubLabel = new GithubLabel({ name: `${category}.................${value}` });
+    expect(githubLabel.isCategorical()).toEqual(true);
+  });
+
+  it ('should return correct values on getCategory() and getValue()', () => {
+    githubLabel = new GithubLabel({ name: `${category}.${value}` });
+    expect(githubLabel.getCategory()).toEqual(category);
+    expect(githubLabel.getValue()).toEqual(value);
+  });
+});
