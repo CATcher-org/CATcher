2bedd142de70686569fbc313a97c6d580883ef39
dingyuchen <dingyuchen@gmail.com>
2020-12-16 21:23:36 +0800

    Mock GithubSerivce in application service tests

--- tests/services/application.service.spec.ts
+++ tests/services/application.service.spec.ts
@@ -1,38 +1,54 @@
 import { ApplicationService } from '../../src/app/core/services/application.service';
+import { GithubService } from '../../src/app/core/services/github.service';
+import { of } from 'rxjs';
 
 const currentVersion = '3.2.5';
 
 class ApplicationServiceStub extends ApplicationService {
   readonly currentVersion: string;
-  constructor(latestVersion: string, currentVersion: string) {
-    super(null);
-    this.latestVersion = latestVersion;
+  constructor(githubService: GithubService, currentVersion: string) {
+    super(githubService);
     this.currentVersion = currentVersion;
   }
 }
 
 describe('Test for ApplicationService#isApplicationOutdated', () => {
-  it('should return an Observable of false if the ApplicationService is outdated', () => {
-    const outdatedAppService1 = new ApplicationServiceStub('2.2.5', currentVersion);
+  const githubService = jasmine.createSpyObj('GithubService', ['fetchLatestRelease']);
+  it('should return the appropriate Observable if the ApplicationService is outdated', () => {
+    githubService.fetchLatestRelease.and.returnValue(of({tag_name: 'v2.2.5'}));
+    const outdatedAppService1 = new ApplicationServiceStub(githubService, currentVersion);
     outdatedAppService1.isApplicationOutdated().subscribe((bool) => expect(bool).toBe(false));
-    const outdatedAppService2 = new ApplicationServiceStub('3.1', currentVersion);
-    outdatedAppService2.isApplicationOutdated().subscribe((bool) => expect(bool).toBe(false));
-    const outdatedAppService3 = new ApplicationServiceStub('3.2.4', currentVersion);
-    outdatedAppService3.isApplicationOutdated().subscribe((bool) => expect(bool).toBe(false));
-    const longVersionedAppService = new ApplicationServiceStub('3.2.4.8.9', currentVersion);
-    longVersionedAppService.isApplicationOutdated().subscribe((bool) => expect(bool).toBe(false));
-    const latestApplicationService = new ApplicationServiceStub(currentVersion, currentVersion);
-    latestApplicationService.isApplicationOutdated().subscribe((bool) => expect(bool).toBe(false));
-  });
 
-  it('should return an Observable of true if the ApplicationService is up to date', () => {
-    const upToDateAppService1 = new ApplicationServiceStub('3.2.5.0', currentVersion);
+    githubService.fetchLatestRelease.and.returnValue(of({tag_name: 'v3.1'}));
+    const outdatedAppService2 = new ApplicationServiceStub(githubService, currentVersion);
+    outdatedAppService2.isApplicationOutdated().subscribe((bool) => expect(bool).toBe(false));
+
+    githubService.fetchLatestRelease.and.returnValue(of({tag_name: 'v3.2.4'}));
+    const outdatedAppService3 = new ApplicationServiceStub(githubService, currentVersion);
+    outdatedAppService3.isApplicationOutdated().subscribe((bool) => expect(bool).toBe(false));
+
+    githubService.fetchLatestRelease.and.returnValue(of({tag_name: 'v3.2.4.8.9'}));
+    const longVersionedAppService = new ApplicationServiceStub(githubService, currentVersion);
+    longVersionedAppService.isApplicationOutdated().subscribe((bool) => expect(bool).toBe(false));
+
+    githubService.fetchLatestRelease.and.returnValue(of({tag_name: 'v3.2.5'}));
+    const latestApplicationService = new ApplicationServiceStub(githubService, currentVersion);
+    latestApplicationService.isApplicationOutdated().subscribe((bool) => expect(bool).toBe(false));
+
+    githubService.fetchLatestRelease.and.returnValue(of({tag_name: 'v3.2.5.0'}));
+    const upToDateAppService1 = new ApplicationServiceStub(githubService, currentVersion);
     upToDateAppService1.isApplicationOutdated().subscribe((bool) => expect(bool).toBe(true));
-    const upToDateAppService2 = new ApplicationServiceStub('3.2.5.5', currentVersion);
+
+    githubService.fetchLatestRelease.and.returnValue(of({tag_name: 'v3.2.5.5'}));
+    const upToDateAppService2 = new ApplicationServiceStub(githubService, currentVersion);
     upToDateAppService2.isApplicationOutdated().subscribe((bool) => expect(bool).toBe(true));
-    const upToDateAppService3 = new ApplicationServiceStub('3.2.6', currentVersion);
+
+    githubService.fetchLatestRelease.and.returnValue(of({tag_name: 'v3.2.6'}));
+    const upToDateAppService3 = new ApplicationServiceStub(githubService, currentVersion);
     upToDateAppService3.isApplicationOutdated().subscribe((bool) => expect(bool).toBe(true));
-    const upToDateAppService4 = new ApplicationServiceStub('3.2.6', currentVersion);
+
+    githubService.fetchLatestRelease.and.returnValue(of({tag_name: 'v10'}));
+    const upToDateAppService4 = new ApplicationServiceStub(githubService, currentVersion);
     upToDateAppService4.isApplicationOutdated().subscribe((bool) => expect(bool).toBe(true));
   });
 });
