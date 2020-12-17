2ca832929b84004e901cf93d2afa152f7a2534b5
branson <ngbranson567@gmail.com>
2020-12-17 20:05:03 +0800

    reword test case

--- tests/app/core/hidden-data-model.spec.ts
+++ tests/app/core/hidden-data-model.spec.ts
@@ -1,30 +1,30 @@
-import { HiddenData } from "../../../src/app/core/models/hidden-data.model";
-const SAMPLE_KEY = "Nunc dolor mauris, rhoncus et facilisis sit amet";
-const SAMPLE_VALUE = "laoreet sit amet sem.";
+import { HiddenData } from '../../../src/app/core/models/hidden-data.model';
+const SAMPLE_KEY = 'Nunc dolor mauris, rhoncus et facilisis sit amet';
+const SAMPLE_VALUE = 'laoreet sit amet sem.';
 const SAMPLE_MAP = new Map<string, string>([[SAMPLE_KEY, SAMPLE_VALUE]]);
 const COMMENT = `<!--${SAMPLE_KEY}: ${SAMPLE_VALUE}-->`;
 
 const embedComment = (
   comment
-) => `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-Nulla viverra nunc ac blandit dictum. Praesent vel est a erat 
-aliquam lobortis. ${comment}Fusce eu metus ex. In tempus erat magna. 
-In hac habitasse platea dictumst. Aenean volutpat nibh mauris, 
+) => `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+Nulla viverra nunc ac blandit dictum. Praesent vel est a erat
+aliquam lobortis. ${comment}Fusce eu metus ex. In tempus erat magna.
+In hac habitasse platea dictumst. Aenean volutpat nibh mauris,
 commodo laoreet risus lacinia vitae.`;
 
-describe("HiddenData", () => {
-  it(".embedDataIntoString should append some specified meta info as HTML comment", () => {
+describe('HiddenData', () => {
+  it('.embedDataIntoString should append meta info as a HTML comment', () => {
     const appendedData = HiddenData.embedDataIntoString(
-      embedComment(""),
+      embedComment(''),
       SAMPLE_MAP
     );
-    expect(appendedData).toEqual(embedComment("") + `\n${COMMENT}`);
+    expect(appendedData).toEqual(embedComment('') + `\n${COMMENT}`);
   });
 
-  it("instance constructor should parse string for HTML comments and generate the right data map", () => {
+  it('constructor should parse string for HTML comments and generate the data map from comments', () => {
     const parsedData = new HiddenData(embedComment(COMMENT));
     expect(parsedData.originalStringWithoutHiddenData).toEqual(
-      embedComment("")
+      embedComment('')
     );
 
     expect(parsedData.toString()).toEqual(COMMENT);
