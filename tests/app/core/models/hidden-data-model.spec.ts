08cc19036e5948953f45fd753a11037c18dc3a23
branson <ngbranson567@gmail.com>
2020-12-21 20:48:41 +0800

    shift test case, added special characters and renamed variable

--- tests/app/core/models/hidden-data-model.spec.ts
+++ tests/app/core/models/hidden-data-model.spec.ts
@@ -1,6 +1,6 @@
-import { HiddenData } from '../../../src/app/core/models/hidden-data.model';
-const SAMPLE_KEY = 'Nunc dolor mauris, rhoncus et facilisis sit amet';
-const SAMPLE_VALUE = 'laoreet sit amet sem.';
+import { HiddenData } from '../../../../src/app/core/models/hidden-data.model';
+const SAMPLE_KEY = 'Nunc dolor mauris, @/;\'12345\'rhoncus "et" <!-> facilisis sit amet';
+const SAMPLE_VALUE = 'laoreet sit @@#$%^&*()_+amet sem.->';
 const SAMPLE_MAP = new Map<string, string>([[SAMPLE_KEY, SAMPLE_VALUE]]);
 const COMMENT = `<!--${SAMPLE_KEY}: ${SAMPLE_VALUE}-->`;
 
@@ -14,11 +14,11 @@ commodo laoreet risus lacinia vitae.`;
 
 describe('HiddenData', () => {
   it('.embedDataIntoString should append meta info as a HTML comment', () => {
-    const appendedData = HiddenData.embedDataIntoString(
+    const dataWithMetaInfo  = HiddenData.embedDataIntoString(
       embedComment(''),
       SAMPLE_MAP
     );
-    expect(appendedData).toEqual(embedComment('') + `\n${COMMENT}`);
+    expect(dataWithMetaInfo ).toEqual(embedComment('') + `\n${COMMENT}`);
   });
 
   it('constructor should parse string for HTML comments and generate the data map from comments', () => {
