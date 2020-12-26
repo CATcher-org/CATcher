db1eb56186dd7b225a2f9de06b2edaa5a7d0f2a9
Jun Wei <kjunwei1996@gmail.com>
2020-12-26 08:42:39 +0800

    Fix profile

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,35 @@
+import { Profile } from '../app/auth/profiles/profiles.component';
+
 export const AppConfig = {
   production: true,
   clientId: '5e1ed08cff7f0de1d68d',
   githubUrl: 'https://github.com',
   accessTokenUrl: 'https://catcher-proxy.herokuapp.com/authenticate',
   origin: 'https://junwei96.github.io',
+  profiles: [
+    <Profile>{
+      profileName: 'CS2103/T Alpha Test',
+      encodedText: 'nus-cs2103-AY2021S2/alpha'
+    },
+    <Profile>{
+      profileName: 'CS2103/T PE Dry run',
+      encodedText: 'nus-cs2103-AY2021S2/PED'
+    },
+    <Profile>{
+      profileName: 'CS2103/T PE',
+      encodedText: 'nus-cs2103-AY2021S2/PE'
+    },
+    <Profile>{
+      profileName: 'CS2113/T Alpha Test',
+      encodedText: 'nus-cs2113-AY2021S2/alpha'
+    },
+    <Profile>{
+      profileName: 'CS2113/T PE Dry run',
+      encodedText: 'nus-cs2113-AY2021S2/PED'
+    },
+    <Profile>{
+      profileName: 'CS2113/T PE',
+      encodedText: 'nus-cs2113-AY2021S2/PE'
+    }
+  ]
 };
