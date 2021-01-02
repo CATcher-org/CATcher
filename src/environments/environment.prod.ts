a3ca5e42ec9a55103b8c6bd05dbbeca6e9738e1f
Jun Wei <kjunwei1996@gmail.com>
2021-01-02 13:11:33 +0800

    Change origin to CATcher-org

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const AppConfig = {
   clientId: '5e1ed08cff7f0de1d68d',
   githubUrl: 'https://github.com',
   accessTokenUrl: 'https://catcher-proxy.herokuapp.com/authenticate',
-  origin: 'https://junwei96.github.io',
+  origin: 'https://CATcher-org.github.io',
   profiles: [
     <Profile>{
       profileName: 'CS2103/T Alpha Test',
