740f4a938b34072576b2c0a9ce173176c6a7a802
anubh-v <anubhav@u.nus.edu>
2021-01-17 20:44:57 +0800

    Use a different reporter, for cleaner printing of results

--- tests/karma.conf.js
+++ tests/karma.conf.js
@@ -9,7 +9,7 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-firefox-launcher'),
-      require('karma-jasmine-html-reporter'),
+      require('karma-spec-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
@@ -21,13 +21,17 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['spec'],
+    specReporter: {
+      maxLogLines: 5, // limit number of lines per test
+      suppressErrorSummary: true,
+      suppressPassed: true,
+      showSpecTiming: false,
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
     browsers: ['ChromeHeadless', 'FirefoxHeadless'],
     singleRun: true,
-    restartOnFileChange: true
   });
 };
