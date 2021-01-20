6822302f9685b76efafc362dbeecd47ae1d0c523
Anubhav <35621759+anubh-v@users.noreply.github.com>
2021-01-20 20:44:45 +0800

    Tests: run using Karma, instead of jasmine-ts (#541)
    Our tests run in the NodeJS environment.
    Let's use Karma to run the tests instead, so
    that we can run them in multiple browser
    environments .

---
+++ tests/karma.ci.conf.js
@@ -0,0 +1,13 @@
+
+let ciProperties;
+let baseConfigs = require('./karma.conf.js');
+baseConfigs({ set: function(arg) { ciProperties = arg; }});
+
+// Set CI specific settings
+// Include Firefox as a testing environment
+ciProperties.plugins.push(require('karma-firefox-launcher'));
+ciProperties.browsers.push('FirefoxHeadless');
+
+module.exports = function(config) {
+  config.set(ciProperties);
+};
