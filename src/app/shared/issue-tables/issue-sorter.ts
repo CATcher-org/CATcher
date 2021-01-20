f00ad27ad0e7770320840e237c7d531f1fa7fc00
Low Jun Kai, Sean <sean.low.jk@gmail.com>
2021-01-20 17:20:38 +0800

    Refactor Tests and Add Constants

--- src/app/shared/issue-tables/issue-sorter.ts
+++ src/app/shared/issue-tables/issue-sorter.ts
@@ -1,39 +1,53 @@
 import { Issue, ISSUE_TYPE_ORDER, SEVERITY_ORDER } from '../../core/models/issue.model';
-import { MatSort, SortDirection } from '@angular/material';
+import { MatSort } from '@angular/material';
 
 export function getSortedData(sort: MatSort, data: Issue[]): Issue[] {
   if (!sort.active) {
       return data;
   }
+
+  const direction: number = (sort.direction === 'asc' ? 1 : -1);
+
   return data.sort((a, b) => {
     switch (sort.active) {
       case 'type':
-        return compareValue(sort.direction, ISSUE_TYPE_ORDER[a.type], ISSUE_TYPE_ORDER[b.type]);
+        return direction * compareByIssueType(a.type, b.type);
       case 'severity':
-        return compareValue(sort.direction, SEVERITY_ORDER[a.severity], SEVERITY_ORDER[b.severity]);
+        return direction * compareBySeverity(a.severity, b.severity);
       case 'assignees':
-        return compareValue(sort.direction, a.assignees.join(', '), b.assignees.join(', '));
+        return direction * compareStringValue(a.assignees.join(', '), b.assignees.join(', '));
       case 'teamAssigned':
-        return compareValue(sort.direction, a.teamAssigned.id, b.teamAssigned.id);
+        return direction * compareStringValue(a.teamAssigned.id, b.teamAssigned.id);
       case 'Todo Remaining':
-        return -compareValue(sort.direction, a.numOfUnresolvedDisputes(), b.numOfUnresolvedDisputes());
-      default: // id, title, responseTag
-        return compareValue(sort.direction, a[sort.active], b[sort.active]);
+        return -direction * compareIntegerValue(a.numOfUnresolvedDisputes(), b.numOfUnresolvedDisputes());
+      case 'id':
+        return direction * compareIntegerValue(a.id, b.id);
+      default: // title, responseTag are string values
+        return direction * compareStringValue(a[sort.active], b[sort.active]);
     }
   });
 }
 
+function compareBySeverity(severityA: string, severityB: string): number {
+  const orderA = SEVERITY_ORDER[severityA];
+  const orderB = SEVERITY_ORDER[severityB];
 
-function compareValue(sortDirection: SortDirection, valueA: string | number, valueB: string | number): number {
-  if (typeof valueA === 'number' && typeof valueB === 'number') {
-    return compareIntegerValue(sortDirection, valueA, valueB);
-  }
-
-  const a = String(valueA || '').toUpperCase();
-  const b = String(valueB || '').toUpperCase();
-  return (a < b ? -1 : 1) * (sortDirection === 'asc' ? 1 : -1);
+  return orderA < orderB ? -1 : 1;
 }
 
-function compareIntegerValue(sortDirection: SortDirection, valueA: number, valueB: number): number {
-  return (valueA < valueB ? -1 : 1) * (sortDirection === 'asc' ? 1 : -1);
+function compareByIssueType(issueTypeA: string, issueTypeB: string): number {
+  const orderA = ISSUE_TYPE_ORDER[issueTypeA];
+  const orderB = ISSUE_TYPE_ORDER[issueTypeB];
+
+  return orderA < orderB ? -1 : 1;
+}
+
+function compareStringValue(valueA: string, valueB: string): number {
+  const orderA = String(valueA || '').toUpperCase();
+  const orderB = String(valueB || '').toUpperCase();
+  return (orderA < orderB ? -1 : 1);
+}
+
+function compareIntegerValue(valueA: number, valueB: number): number {
+  return (valueA < valueB ? -1 : 1);
 }
--- tests/app/shared/issue-tables/issue-sorter.spec.ts
+++ tests/app/shared/issue-tables/issue-sorter.spec.ts
@@ -1,14 +1,36 @@
 import { getSortedData } from '../../../../src/app/shared/issue-tables/issue-sorter';
 import { MatSort } from '@angular/material';
 import { Issue } from '../../../../src/app/core/models/issue.model';
-import { ISSUE_WITH_ASSIGNEES, ISSUE_WITH_EMPTY_DESCRIPTION } from '../../../constants/githubissue.constants';
+import { ISSUE_WITH_ASSIGNEES, ISSUE_WITH_EMPTY_DESCRIPTION, ISSUE_PENDING_MODERATION } from '../../../constants/githubissue.constants';
+import { Team } from '../../../../src/app/core/models/team.model';
 
 describe('issuer-sorter', () => {
   describe('getSortedData()', () => {
-    const dummyIssue: Issue = Issue.createPhaseBugReportingIssue(ISSUE_WITH_EMPTY_DESCRIPTION);
-    const otherDummyIssue: Issue = Issue.createPhaseBugReportingIssue(ISSUE_WITH_ASSIGNEES);
+    const dummyTeam = new Team({
+      id: 'F09-2',
+      teamMembers: [],
+    });
+    const dummyIssue: Issue = Issue.createPhaseTeamResponseIssue(ISSUE_WITH_EMPTY_DESCRIPTION, dummyTeam);
+    const otherDummyIssue: Issue = Issue.createPhaseTeamResponseIssue(ISSUE_WITH_ASSIGNEES, dummyTeam);
     const issuesList: Issue[] = [dummyIssue, otherDummyIssue];
+
+    const dummyTodoIssue: Issue = Issue.createPhaseModerationIssue(ISSUE_WITH_EMPTY_DESCRIPTION, dummyTeam);
+    const otherDummTodoIssue: Issue = Issue.createPhaseModerationIssue(ISSUE_PENDING_MODERATION, dummyTeam);
+    const todoIssuesList: Issue[] = [dummyTodoIssue, otherDummTodoIssue];
     const matSort: MatSort = new MatSort();
+
+    it('sorts issues based on their assignees correctly', () => {
+      matSort.active = 'assignees';
+      matSort.direction = 'asc';
+      const sortedIssuesByTitleAsc = getSortedData(matSort, issuesList);
+      expect(sortedIssuesByTitleAsc[0].id).toBe(dummyIssue.id);
+      expect(sortedIssuesByTitleAsc[1].id).toBe(otherDummyIssue.id);
+
+      matSort.direction = 'desc';
+      const sortedIssuesByTitleDesc = getSortedData(matSort, issuesList);
+      expect(sortedIssuesByTitleDesc[0].id).toBe(otherDummyIssue.id);
+      expect(sortedIssuesByTitleDesc[1].id).toBe(dummyIssue.id);
+    });
 
     it('sorts issues based on their string fields correctly', () => {
         matSort.active = 'title';
@@ -23,7 +45,7 @@ describe('issuer-sorter', () => {
         expect(sortedIssuesByTitleDesc[1].id).toBe(dummyIssue.id);
     });
 
-    it('sorts issues based on their string fields correctly', () => {
+    it('sorts issues based on their integer fields correctly', () => {
         matSort.active = 'id';
         matSort.direction = 'asc';
         const sortedIssuedByIdAsc = getSortedData(matSort, issuesList);
@@ -34,6 +56,19 @@ describe('issuer-sorter', () => {
         const sortedIssuedByIdDesc = getSortedData(matSort, issuesList);
         expect(sortedIssuedByIdDesc[0].id).toBe(dummyIssue.id);
         expect(sortedIssuedByIdDesc[1].id).toBe(otherDummyIssue.id);
+    });
+
+    it('sorts issues based on their todos left correctly', () => {
+      matSort.active = 'Todo Remaining';
+      matSort.direction = 'asc';
+      const sortedIssuesByTitleAsc = getSortedData(matSort, todoIssuesList);
+      expect(sortedIssuesByTitleAsc[0].id).toBe(otherDummTodoIssue.id);
+      expect(sortedIssuesByTitleAsc[1].id).toBe(dummyTodoIssue.id);
+
+      matSort.direction = 'desc';
+      const sortedIssuesByTitleDesc = getSortedData(matSort, todoIssuesList);
+      expect(sortedIssuesByTitleDesc[0].id).toBe(dummyTodoIssue.id);
+      expect(sortedIssuesByTitleDesc[1].id).toBe(otherDummTodoIssue.id);
     });
   });
 });
