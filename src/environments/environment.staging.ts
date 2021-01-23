36da267dc3d2fca122ede6abcd60dd19d2fb8125
Sean Low <sean.low.jk@gmail.com>
2021-01-23 18:57:34 +0800

    Use Deconstruction for staging config

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,4 +1,6 @@
 import { AppConfig as ProdAppConfig } from './environment.prod';
 
-ProdAppConfig.clientId = '54b9dcc49069dc2f018e';
-export const AppConfig = ProdAppConfig;
+export const AppConfig = {
+  clientId: '54b9dcc49069dc2f018e',
+  ...ProdAppConfig
+};
