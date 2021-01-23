2537fe841c3e35006da07025232cc5c9f0b8c4cc
Sean Low <sean.low.jk@gmail.com>
2021-01-23 19:14:15 +0800

    Flip clientId

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,6 +1,6 @@
 import { AppConfig as ProdAppConfig } from './environment.prod';
 
 export const AppConfig = {
-  clientId: '54b9dcc49069dc2f018e',
-  ...ProdAppConfig
+  ...ProdAppConfig, 
+  clientId: '54b9dcc49069dc2f018e'
 };
