de4bf0ef5eaf78fcedce48cf195d309456c5a89b
Low Jun Kai, Sean <sean.low.jk@gmail.com>
2021-01-27 22:49:10 +0800

    Refix Staging and Production Configurations

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -2,5 +2,6 @@ import { AppConfig as ProdAppConfig } from './environment.prod';
 
 export const AppConfig = {
   ...ProdAppConfig,
-  clientId: '54b9dcc49069dc2f018e'
+  clientId: '54b9dcc49069dc2f018e',
+  origin: 'https://catcher-org.github.io',
 };
