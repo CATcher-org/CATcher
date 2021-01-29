6127cdfec7c073be76db430b66ba198855d0dc08
Su <kkangs0226@gmail.com>
2021-01-29 18:39:37 +0800

    Fix lint errors

--- tests/app/shared/issue-tables/issue-tables.component.spec.ts
+++ tests/app/shared/issue-tables/issue-tables.component.spec.ts
@@ -1,4 +1,4 @@
-import { IssueTablesComponent } from '../../../../src/app/shared/issue-tables/issue-tables.component'
+import { IssueTablesComponent } from '../../../../src/app/shared/issue-tables/issue-tables.component';
 
 describe('IssueTablesComponent', () => {
     let issueTablesComponent: IssueTablesComponent;
@@ -9,21 +9,20 @@ describe('IssueTablesComponent', () => {
         });
 
         it('should format the title text to account for words longer than max word length', () => {
-            //title consisting of 2 words of 50 characters
+            // title consisting of 2 words of 50 characters
             const LONG_TITLE = '01234567890123456789012345678901234567890123456789 01234567890123456789012345678901234567890123456789';
-            const FORMATTED_TITLE = '01234567890123456789012345678901234567... 01234567890123456789012345678901234567...'
+            const FORMATTED_TITLE = '01234567890123456789012345678901234567... 01234567890123456789012345678901234567...';
             expect(issueTablesComponent.fitTitleText(LONG_TITLE)).toEqual(FORMATTED_TITLE);
         });
 
         it('should retain original title if words are not longer than max word length', () => {
-            //title consisting of 2 words of 30 characters
+            // title consisting of 2 words of 30 characters
             const SHORT_TITLE = '012345678901234567890123456789 012345678901234567890123456789';
             expect(issueTablesComponent.fitTitleText(SHORT_TITLE)).toEqual(SHORT_TITLE);
 
-            //title consisting of 2 words of 43 characters (maximum word length)
+            // title consisting of 2 words of 43 characters (maximum word length)
             const MAX_LENGTH_TITLE = '0123456789012345678901234567890123456789012 0123456789012345678901234567890123456789012';
             expect(issueTablesComponent.fitTitleText(MAX_LENGTH_TITLE)).toEqual(MAX_LENGTH_TITLE);
         });
-    })
-    
+    });
 });
