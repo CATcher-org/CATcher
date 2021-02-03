abaae6796787288929a7ffecdbaa2a1bff832f3c
dingyuchen <dingyuchen@gmail.com>
2021-02-03 22:37:44 +0800

    Update SessionData interface with enumerated phases, improve tests

--- tests/app/core/models/session-model.spec.ts
+++ tests/app/core/models/session-model.spec.ts
@@ -1,56 +1,61 @@
-import { assertSessionDataIntegrity } from '../../../../src/app/core/models/session.model';
-import { of } from 'rxjs';
+import {
+  assertSessionDataIntegrity,
+  SessionData,
+  SESSION_DATA_INCORRECTLY_DEFINED,
+  SESSION_DATA_UNAVAILABLE,
+  NO_ACCESSIBLE_PHASES,
+} from "../../../../src/app/core/models/session.model";
+import { Phase } from "../../../../src/app/core/services/phase.service";
+import { of } from "rxjs";
 
-const validSessionData = {
+const validSessionData: SessionData = {
   openPhases: [
-    'phaseBugReporting',
-    'phaseTeamResponse',
-    'phaseTesterResponse',
-    'phaseModeration',
+    Phase.phaseBugReporting,
+    Phase.phaseTeamResponse,
+    Phase.phaseTesterResponse,
+    Phase.phaseModeration,
   ],
-  phaseBugReporting: 'bugreporting',
-  phaseTeamResponse: 'pe-results',
-  phaseTesterResponse: 'testerresponse',
-  phaseModeration: 'pe-evaluation',
+  phaseBugReporting: "bugreporting",
+  phaseTeamResponse: "pe-results",
+  phaseTesterResponse: "testerresponse",
+  phaseModeration: "pe-evaluation",
 };
 
-describe('Session Model', () => {
-  describe('Assert Session Data Integrity', () => {
-    it('should throw error on unavailable session', () => {
+describe("Session Model", () => {
+  describe("assertSessionDataIntegrity", () => {
+    it("should throw error on unavailable session", () => {
       of(undefined)
         .pipe(assertSessionDataIntegrity())
         .subscribe({
           error: (err) =>
-            expect(err).toEqual(new Error('Session Data Unavailable')),
+            expect(err).toEqual(new Error(SESSION_DATA_UNAVAILABLE)),
         });
     });
-    it('should throw error on incorrect session data', () => {
-      of({ key: '' })
+
+    it("should throw error on session data with missing values", () => {
+      of({ key: "" })
         .pipe(assertSessionDataIntegrity())
         .subscribe({
           error: (err) =>
-            expect(err).toEqual(
-              new Error('Session Data is Incorrectly Defined')
-            ),
+            expect(err).toEqual(new Error(SESSION_DATA_INCORRECTLY_DEFINED)),
         });
       of({ key: undefined })
         .pipe(assertSessionDataIntegrity())
         .subscribe({
           error: (err) =>
-            expect(err).toEqual(
-              new Error('Session Data is Incorrectly Defined')
-            ),
+            expect(err).toEqual(new Error(SESSION_DATA_INCORRECTLY_DEFINED)),
         });
     });
-    it('should throw error on session with no open phases', () => {
+
+    it("should throw error on session with no open phases", () => {
       of({ openPhases: [] })
         .pipe(assertSessionDataIntegrity())
         .subscribe({
-          error: (err) =>
-            expect(err).toEqual(new Error('There are no accessible phases.')),
+          error: (err) => expect(err).toEqual(new Error(NO_ACCESSIBLE_PHASES)),
         });
     });
-    it('should pass valid session data', () => {
+
+    it("should pass valid session data", () => {
       of(validSessionData)
         .pipe(assertSessionDataIntegrity())
         .subscribe((el) => expect(el).toEqual(validSessionData));
