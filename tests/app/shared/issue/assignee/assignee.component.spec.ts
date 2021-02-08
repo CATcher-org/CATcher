ff707ff60db8405da36024ecde939327e33a223e
Low Jun Kai, Sean <sean.low.jk@gmail.com>
2021-02-08 16:26:10 +0800

    Fix Necessary HTML Component Tests

--- tests/app/shared/issue/assignee/assignee.component.spec.ts
+++ tests/app/shared/issue/assignee/assignee.component.spec.ts
@@ -2,9 +2,7 @@ import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { Phase, PhaseService } from '../../../../../src/app/core/services/phase.service';
 import { User, UserRole } from '../../../../../src/app/core/models/user.model';
 
-import {
-  ApolloTestingModule,
-} from 'apollo-angular/testing';
+import { ApolloTestingModule } from 'apollo-angular/testing';
 import { AssigneeComponent } from '../../../../../src/app/shared/issue/assignee/assignee.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { By } from '@angular/platform-browser';
@@ -20,6 +18,7 @@ import { MaterialModule } from '../../../../../src/app/shared/material.module';
 import { PermissionService } from '../../../../../src/app/core/services/permission.service';
 import { Team } from '../../../../../src/app/core/models/team.model';
 import { UserService } from '../../../../../src/app/core/services/user.service';
+import { of } from 'rxjs';
 
 describe('AssigneeComponent', () => {
   let component: AssigneeComponent;
@@ -36,14 +35,14 @@ describe('AssigneeComponent', () => {
       id: 'F09-2',
       teamMembers: [ testStudent ],
   });
+  
   const dummyIssue: Issue =  Issue.createPhaseTeamResponseIssue(ISSUE_WITH_EMPTY_DESCRIPTION, dummyTeam);
 
   const userService: UserService = new UserService(null, null);
   userService.currentUser = testStudent;
   const phaseService: PhaseService = new PhaseService(null, null, null, userService, null);
   phaseService.currentPhase = Phase.phaseTeamResponse;
-  const permissionsService: PermissionService = new PermissionService(null, userService, phaseService);
-  const issueService: IssueService = new IssueService(null, userService, phaseService, permissionsService, null, null);
+  const issueService = jasmine.createSpyObj('IssueService', ['getLatestIssue', 'updateIssue']);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -60,7 +59,6 @@ describe('AssigneeComponent', () => {
     .overrideProvider(UserService, { useValue: userService })
     .overrideProvider(PhaseService, { useValue: phaseService })
     .overrideProvider(IssueService, { useValue: issueService })
-    .overrideProvider(PermissionService, { useValue: permissionsService })
     .compileComponents();
   }));
 
@@ -73,7 +71,6 @@ describe('AssigneeComponent', () => {
     component.team = dummyTeam;
     component.issue = dummyIssue;
     fixture.detectChanges();
-
     debugElement = fixture.debugElement;
   });
 
@@ -81,8 +78,7 @@ describe('AssigneeComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should be able to see the placeholder value', () => {
-    // Expect a Placeholder Value to be defined if no assignees
+  it('should have a placeholder value of - given no assignees', () => {
     const matPlaceholderValue: HTMLElement = debugElement.query(By.css('.mat-select-placeholder')).nativeElement;
     expect(matPlaceholderValue).toBeDefined();
     expect(matPlaceholderValue.innerText).toEqual('-'); // Placeholder Value
@@ -91,7 +87,7 @@ describe('AssigneeComponent', () => {
   it('should be able to open the assignee selector', () => {
     const matSelect: HTMLElement = debugElement.query(By.css('.mat-select-trigger')).nativeElement;
 
-    // Open the assignee Selector
+    // Open the assignee selector
     matSelect.click();
     fixture.detectChanges();
     const matOption: HTMLElement = debugElement.query(By.css('.mat-option')).nativeElement;
@@ -103,7 +99,24 @@ describe('AssigneeComponent', () => {
     expect(matOptionAttributes.getNamedItem('aria-selected').value).toEqual('false');
   });
 
-  it('should be able to assign a new assignee', () => {
+  it('should emit the issueUpdated event upon closing the MatSelect', () => {
+    spyOn(component.issueUpdated, 'emit');
+    const matSelect: HTMLElement = debugElement.query(By.css('.mat-select-trigger')).nativeElement;
+
+    // Open the assignee selector
+    matSelect.click();
+    fixture.detectChanges();
+
+    // Close the matSelect element and make relevant fake calls. 
+    const matSelectElement = debugElement.query(By.css('.mat-select')).nativeElement;
+    issueService.updateIssue.and.callFake(() => of(dummyIssue));
+    matSelectElement.dispatchEvent(new Event('closed'));
+    fixture.detectChanges();
+
+    expect(component.issueUpdated.emit).toHaveBeenCalledTimes(1);
+  });
+
+  it('should show the new assignee value upon adding a new assignee', () => {
     const matSelect: HTMLElement = debugElement.query(By.css('.mat-select-trigger')).nativeElement;
 
     // Open the Assignee Selector
@@ -116,9 +129,15 @@ describe('AssigneeComponent', () => {
     matOption.click();
     fixture.detectChanges();
     expect(matOptionAttributes.getNamedItem('aria-selected').value).toEqual('true');
-    expect(component.assignees).toEqual(component.teamMembers);
 
-    const textValue: HTMLElement = debugElement.query(By.css('.mat-select-value-text')).nativeElement;
-    expect(textValue).toBeDefined();
+    // Close the matSelect element and make relevant fake calls. 
+    const matSelectElement = debugElement.query(By.css('.mat-select')).nativeElement;
+    issueService.updateIssue.and.callFake(() => of(dummyIssue));
+    matSelectElement.dispatchEvent(new Event('closed'));
+    component.issue.assignees = [ testStudent.loginId ];
+    fixture.detectChanges();
+
+    const matListText: HTMLElement = debugElement.query(By.css('.mat-list-item-content')).nativeElement;
+    expect(matListText.innerText).toEqual(component.assignees[0]);
   });
 });
