32b64cb071a1fd7ba58a2a86f531fba38571fa25
Prithivi Raj <ptvrajsk@gmail.com>
2021-02-12 16:59:58 +0800

    Undo Fix in This PR

--- e2e/spec/bugReporting/bugReporting.e2e-spec.ts
+++ e2e/spec/bugReporting/bugReporting.e2e-spec.ts
@@ -1,6 +1,5 @@
 import { BugReportingPage } from '../../page-objects/bugReporting.po';
-import { PhaseDescription } from '../../../src/app/core/services/phase.service';
-import { Phase } from '../../../src/app/core/models/phase.model';
+import { Phase, PhaseDescription } from '../../../src/app/core/services/phase.service';
 import { LoginPage } from '../../page-objects/login.po';
 
 describe('CATcher\'s Bug Reporting Page', () => {
