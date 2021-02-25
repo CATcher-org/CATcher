514998b4c40565a8e56c63080b893b3a075ab192
Sean Low <sean.low.jk@gmail.com>
2021-02-25 23:45:50 +0800

    Create Repository Service and Fix Tests

---
+++ src/app/core/services/repo-creator.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { flatMap } from 'rxjs/operators';
+import { Observable, of, pipe, UnaryFunction } from 'rxjs';
+import { GithubService } from './github.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RepoCreatorService {
+  constructor(
+    private githubService: GithubService
+  ) {}
+
+  public verifyRepoCreation(phaseOwner: string, phaseRepo: string): UnaryFunction<Observable<boolean | null>, Observable<boolean>> {
+    return pipe(
+      flatMap((isFixAttempted: boolean | null) => {
+        if (isFixAttempted === null) {
+          // If no fix has been attempted, there is no need to verify fix outcome.
+          return of(true);
+        } else if (isFixAttempted === true) {
+          // Verify that Repository has been created if a fix attempt has occurred.
+          return this.verifySessionAvailability(phaseOwner, phaseRepo);
+        }
+      })
+    );
+  }
+
+    /**
+   * Checks if the necessary repository is available and creates it if the permissions are available.
+   * @param sessionData
+   */
+  private verifySessionAvailability(phaseOwner: string, phaseRepo: string): Observable<boolean> {
+    return this.githubService.isRepositoryPresent(phaseOwner, phaseRepo);
+  }
+
+}
+
