09059d07bf16394cf183ddc663afa6a7b745e572
Kang Su Min <66008784+kkangs0226@users.noreply.github.com>
2021-02-26 22:21:39 +0800

    IssuesDataTable: Move issue search into separate module, add tests (#593)
    Logic for applying search filter to the table of issues is defined 
    in a private method within IssuesDataTable.
    
    Moving this logic to another module will improve separation of 
    concerns. Adding tests for this logic would help with future
    regression testing. 
    
    Let's
    * Move logic for search filter to another module
    * Add tests for the module

--- src/app/shared/issue-tables/issue-tables.component.ts
+++ src/app/shared/issue-tables/issue-tables.component.ts
@@ -20,19 +20,6 @@ export enum ACTION_BUTTONS {
   DELETE_ISSUE
 }
 
-export enum TABLE_COLUMNS {
-  ID = 'id',
-  TITLE = 'title',
-  TEAM_ASSIGNED = 'teamAssigned',
-  TYPE = 'type',
-  SEVERITY = 'severity',
-  RESPONSE = 'responseTag',
-  ASSIGNEE = 'assignees',
-  DUPLICATED_ISSUES = 'duplicatedIssues',
-  TODO = 'Todo Remaining',
-  ACTIONS = 'actions'
-}
-
 @Component({
   selector: 'app-issue-tables',
   templateUrl: './issue-tables.component.html',
