5bc3c5d4f16668accc698dc68a9a65c2c8473008
Prithivi Raj <ptvrajsk@gmail.com>
2021-03-01 14:36:03 +0800

    localStorage POC

--- src/app/core/services/logging.service.ts
+++ src/app/core/services/logging.service.ts
@@ -7,6 +7,7 @@ import { ElectronLog } from 'electron-log';
 })
 export class LoggingService {
   private logger: ElectronLog | Console;
+  private readonly LOG_KEY = 'CATcher-Log';
 
   constructor(electronService: ElectronService) {
     if (electronService.isElectron()) {
@@ -16,8 +17,19 @@ export class LoggingService {
     }
   }
 
+  saveToLocalStorage(...params: any[]) {
+    console.log('incoming params');
+    console.log(params);
+    const existingLog = localStorage.getItem(this.LOG_KEY);
+    localStorage.setItem(this.LOG_KEY, params.toString());
+    console.log('existing params');
+    console.log(existingLog);
+  }
+
   info(...params: any[]) {
+    this.saveToLocalStorage(params);
     this.logger.info(params);
+    this.saveToLocalStorage(params);
   }
 
   error(...params: any[]) {
