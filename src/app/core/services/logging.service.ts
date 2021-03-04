8f851ca9b255f7448c09dfff7a7fd9d9badb22d9
Prithivi Raj <ptvrajsk@gmail.com>
2021-03-04 17:39:16 +0800

    Added LogDownload Cap

--- src/app/core/services/logging.service.ts
+++ src/app/core/services/logging.service.ts
@@ -8,6 +8,7 @@ import { ElectronLog } from 'electron-log';
 export class LoggingService {
   private logger: ElectronLog | Console;
   private readonly LOG_KEY = 'CATcher-Log';
+  private readonly LOG_FILE_NAME = 'CATcher-log.txt'
 
   constructor(electronService: ElectronService) {
     if (electronService.isElectron()) {
@@ -17,11 +18,39 @@ export class LoggingService {
     }
   }
 
+  getCachedLog(): string {
+    return localStorage.getItem(this.LOG_KEY);
+  }
+
+  setCachedLog(updatedLog: string): void {
+    localStorage.setItem(this.LOG_KEY, updatedLog);
+  }
+
   saveToLocalStorage(...params: any[]) {
     const oldestSessionLogRegex = /(AppConfig,.*\n[\S\s]*)(?=AppConfig,.*)/g;
-    const existingLog = localStorage.getItem(this.LOG_KEY);
+    const existingLog = this.getCachedLog();
     const newLog = `${existingLog ? `${existingLog}\n` : ''}${params.toString()}`.replace(oldestSessionLogRegex, '');
-    localStorage.setItem(this.LOG_KEY, newLog);
+    this.setCachedLog(newLog);
+  }
+
+  exportLogFile() {
+    const log: string = this.getCachedLog();
+    const blob: Blob = new Blob([log], {type: 'file/txt'});
+    const blobUrl: string = window.URL.createObjectURL(blob);
+
+    const hiddenElement: HTMLAnchorElement = document.createElement('a');
+    hiddenElement.setAttribute('style', 'display: none;');
+    hiddenElement.href = blobUrl;
+    hiddenElement.download = this.LOG_FILE_NAME;
+    
+    // Add to DOM and Click to prompt download.
+    document.body.appendChild(hiddenElement);
+    hiddenElement.click();
+
+    // Remove URL + Created Attached Element
+    window.URL.revokeObjectURL(blobUrl);
+    document.body.removeChild(hiddenElement);
+    hiddenElement.remove();
   }
 
   info(...params: any[]) {
