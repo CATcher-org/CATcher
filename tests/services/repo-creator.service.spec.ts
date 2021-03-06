e802ea3b0102551ed221c2a42cd7251bffc90947
Sean Low <sean.low.jk@gmail.com>
2021-03-07 01:41:46 +0800

    Fix Linting

--- tests/services/repo-creator.service.spec.ts
+++ tests/services/repo-creator.service.spec.ts
@@ -14,18 +14,14 @@ describe('RepoCreatorService', () => {
 
   describe('.verifyRepoCreation()', () => {
     it('should not need to check the presence of the repository if no fix was done', () => {
-      of(null)
-        .pipe(repoCreatorService.verifyRepoCreation(PHASE_OWNER, PHASE_REPO))
-        .subscribe();
+      of(null).pipe(repoCreatorService.verifyRepoCreation(PHASE_OWNER, PHASE_REPO)).subscribe();
 
       expect(githubService.isRepositoryPresent).not.toHaveBeenCalled();
     });
 
     it('should check the presence of the repository if a fix was done', () => {
       githubService.isRepositoryPresent.and.callFake(() => of(true));
-      of(true)
-        .pipe(repoCreatorService.verifyRepoCreation(PHASE_OWNER, PHASE_REPO))
-        .subscribe();
+      of(true).pipe(repoCreatorService.verifyRepoCreation(PHASE_OWNER, PHASE_REPO)).subscribe();
 
       expect(githubService.isRepositoryPresent).toHaveBeenCalledWith(PHASE_OWNER, PHASE_REPO);
     });
