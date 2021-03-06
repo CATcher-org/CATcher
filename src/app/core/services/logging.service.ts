016b3f84e0e16de0a544094355c10958309a54c9
Prithivi Raj <ptvrajsk@gmail.com>
2021-03-06 18:53:42 +0800

    Complete Suite

--- src/app/core/services/logging.service.ts
+++ src/app/core/services/logging.service.ts
@@ -9,6 +9,8 @@ export class LoggingService {
   private logger: ElectronLog | Console;
   private readonly LOG_KEY = 'CATcher-Log';
   private readonly LOG_FILE_NAME = 'CATcher-log.txt';
+  private readonly LOG_START_HEADER = '====== Initializing CATcher Log ======'
+  private readonly LOG_COUNT_LIMIT = 4;
 
   constructor(electronService: ElectronService) {
     if (electronService.isElectron()) {
@@ -16,6 +18,44 @@ export class LoggingService {
     } else {
       this.logger = console;
     }
+
+    this.initializeLogCache();
+  }
+
+  initializeLogCache() {
+    this.setCachedLog(this.getTrimmedLogCache(this.getCachedLog(), this.LOG_COUNT_LIMIT));
+  }
+
+  /**
+   * Trims the existing Log in the browser's cache to a select number
+   * of Sessions if necessary.
+   * @param sessionCount The number of Session Logs to preserve in the cache
+   */
+  getTrimmedLogCache(currentLog: string, sessionCount: number): string {
+    // Check if Trimming is Necessary
+    const numberOfSessions: number = currentLog.split('\n')
+      .filter((currentLogLine: string) => currentLogLine.includes(this.LOG_START_HEADER))
+      .length;
+
+    if(numberOfSessions < sessionCount) {
+      return `${currentLog}\n${this.LOG_START_HEADER}`;
+    }
+
+    const seperatedSessionLogs: string[] = [''];
+    currentLog.split('\n')
+      .forEach((currentLogLine: string) => {
+        if(currentLogLine === this.LOG_START_HEADER) {
+          seperatedSessionLogs.push(currentLogLine);
+        } else {
+          seperatedSessionLogs[seperatedSessionLogs.length - 1] += `\n${currentLogLine}`;
+        }
+      });
+    
+    seperatedSessionLogs.push(this.LOG_START_HEADER);
+    seperatedSessionLogs.splice(0, seperatedSessionLogs.length - sessionCount);
+    return seperatedSessionLogs.reduce((mergedLog: string, currentSessionLog: string) => {
+      return `${mergedLog}\n${currentSessionLog}`;
+    })
   }
 
   getCachedLog(): string {
@@ -26,11 +66,8 @@ export class LoggingService {
     localStorage.setItem(this.LOG_KEY, updatedLog);
   }
 
-  saveToLocalStorage(...params: any[]) {
-    const oldestSessionLogRegex = /(AppConfig,.*\n[\S\s]*)(?=AppConfig,.*)/g;
-    const existingLog = this.getCachedLog();
-    const newLog = `${existingLog ? `${existingLog}\n` : ''}${params.toString()}`.replace(oldestSessionLogRegex, '');
-    this.setCachedLog(newLog);
+  updateLog(... updatedLog: any[]): void {
+    this.setCachedLog(`${this.getCachedLog()}\n${updatedLog.toString()}`);
   }
 
   exportLogFile() {
@@ -54,22 +91,22 @@ export class LoggingService {
   }
 
   info(...params: any[]) {
-    this.saveToLocalStorage(params);
+    this.updateLog(params);
     this.logger.info(params);
   }
 
   error(...params: any[]) {
-    this.saveToLocalStorage(params);
+    this.updateLog(params);
     this.logger.error(params);
   }
 
   warn(...params: any[]) {
-    this.saveToLocalStorage(params);
+    this.updateLog(params);
     this.logger.warn(params);
   }
 
   debug(...params: any[]) {
-    this.saveToLocalStorage(params);
+    this.updateLog(params);
     this.logger.debug(params);
   }
 }
