b521b02f7e9479856377f9325a046d50f135d5f3
Sean Low <sean.low.jk@gmail.com>
2021-03-08 18:04:14 +0800

    Test Custom Laucncher for HeadlessChrome for Karma Config

--- tests/karma.conf.js
+++ tests/karma.conf.js
@@ -30,7 +30,13 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    browsers: ['ChromeHeadless'],
+    browsers: ['HeadlessChrome'],
+    customLaunchers:{
+        HeadlessChrome:{
+            base: 'ChromeHeadless',
+            flags: ['--no-sandbox']
+        }
+    },
     singleRun: true
   });
 };
