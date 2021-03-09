36eac8d59140bdec0a647987a7b789c38d270b9c
dingyuchen <dingyuchen@gmail.com>
2021-03-09 11:21:50 +0800

    Add tests for isValidProfile

---
+++ tests/model/profile.model.spec.ts
@@ -0,0 +1,28 @@
+import { Profile, isValidProfile } from '../../src/app/core/models/profile.model';
+
+describe('isValidProfile', () => {
+  it('returns false for a profile with no encodedText field', () => {
+    const profile = { profileName: 'CATcher-org' };
+    expect(isValidProfile(profile as Profile)).toBe(false);
+  });
+
+  it('returns false for a profile with an empty encodedText field', () => {
+    const profile = { profileName: 'CATcher-org', encodedText: '' };
+    expect(isValidProfile(profile as Profile)).toBe(false);
+  });
+
+  it('returns false for a profile with no profileName field', () => {
+    const profile = { encodedText: 'public' };
+    expect(isValidProfile(profile as Profile)).toBe(false);
+  });
+
+  it('returns false for a profile with an empty profileName field', () => {
+    const profile = { profileName: '', encodedText: 'public' };
+    expect(isValidProfile(profile as Profile)).toBe(false);
+  });
+
+  it('returns true for a valid profile with profileName and encodedText fields', () => {
+    const profile = { profileName: 'CATcher-org', encodedText: 'public' };
+    expect(isValidProfile(profile as Profile)).toBe(true);
+  });
+});
