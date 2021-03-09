a82159e27cc9be5a7190039503ca2534dedcfbf6
Sean Low <sean.low.jk@gmail.com>
2021-03-09 18:10:45 +0800

    Add Basic Tests for attemptRepoCreation

--- tests/services/repo-creator.service.spec.ts
+++ tests/services/repo-creator.service.spec.ts
@@ -1,6 +1,8 @@
-import { RepoCreatorService } from '../../src/app/core/services/repo-creator.service';
+import { MISSING_REQUIRED_REPO, RepoCreatorService } from '../../src/app/core/services/repo-creator.service';
 import { of } from 'rxjs';
 import { UserService } from '../../src/app/core/services/user.service';
+import { Phase } from '../../src/app/core/models/phase.model';
+import { USER_JUNWEI } from '../constants/data.constants';
 
 const PHASE_OWNER = 'CATcher-org';
 const PHASE_REPO = 'bugreporting';
@@ -11,7 +13,7 @@ let userService: UserService;
 describe('RepoCreatorService', () => {
   beforeEach(() => {
     userService = new UserService(null, null);
-    githubService = jasmine.createSpyObj('GithubService', ['isRepositoryPresent']);
+    githubService = jasmine.createSpyObj('GithubService', ['isRepositoryPresent', 'createRepository']);
     repoCreatorService = new RepoCreatorService(githubService, userService);
   });
 
@@ -27,6 +29,33 @@ describe('RepoCreatorService', () => {
       of(true).pipe(repoCreatorService.verifyRepoCreation(PHASE_OWNER, PHASE_REPO)).subscribe();
 
       expect(githubService.isRepositoryPresent).toHaveBeenCalledWith(PHASE_OWNER, PHASE_REPO);
+    });
+  });
+
+  describe('.attemptRepoCreation()', () => {
+    it('should not need to create the repository if no permissions were needed', () => {
+      of(null).pipe(repoCreatorService.attemptRepoCreation(Phase.phaseBugReporting, PHASE_REPO)).subscribe();
+
+      expect(githubService.createRepository).not.toHaveBeenCalled();
+    });
+
+    it('should create the repository if permissions were given', () => {
+      userService.currentUser = USER_JUNWEI;
+      githubService.createRepository.and.callFake(() => of(true));
+      of(true).pipe(repoCreatorService.attemptRepoCreation(Phase.phaseBugReporting, PHASE_REPO)).subscribe();
+
+      expect(githubService.createRepository).toHaveBeenCalledWith(PHASE_REPO);
+    });
+
+    it('should throw an error if no permissions were given', () => {
+      of(false)
+        .pipe(repoCreatorService.attemptRepoCreation(Phase.phaseBugReporting, PHASE_REPO))
+        .subscribe({
+          next: () => fail(),
+          error: (err) => expect(err).toEqual(new Error(MISSING_REQUIRED_REPO))
+        });
+
+      expect(githubService.createRepository).not.toHaveBeenCalled();
     });
   });
 });
