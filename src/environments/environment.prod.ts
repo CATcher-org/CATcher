4adbe299b5adb1a27167bc57ee0c7f64d584cec6
Prithivi Raj <ptvrajsk@gmail.com>
2021-03-10 17:41:55 +0800

    Production Profiles Update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -31,6 +31,18 @@ export const AppConfig = {
     <Profile>{
       profileName: 'CS2113/T PE',
       encodedText: 'nus-cs2113-AY2021S2/PE'
+    },
+    <Profile>{
+      profileName: 'TIC4002 Alpha Test',
+      encodedText: 'nus-tic4002-AY2021S2/alpha'
+    },
+    <Profile>{
+      profileName: 'TIC4002 PE Dry run',
+      encodedText: 'nus-tic4002-AY2021S2/PED'
+    },
+    <Profile>{
+      profileName: 'TIC4002 PE',
+      encodedText: 'nus-tic4002-AY2021S2/PE'
     }
   ]
 };
