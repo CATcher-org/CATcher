316ab041d5015b61b98eac98af02c772da76cab3
Ding YuChen <dingyuchen@gmail.com>
2021-03-12 19:47:17 +0800

    Refactor supported file types
    Co-authored-by: ptvrajsk <39243082+ptvrajsk@users.noreply.github.com>

--- src/app/core/services/upload.service.ts
+++ src/app/core/services/upload.service.ts
@@ -4,8 +4,7 @@ import { uuid } from '../../shared/lib/uuid';
 import { throwError } from 'rxjs';
 
 const SUPPORTED_VIDEO_FILE_TYPES = ['mp4', 'mov'];
-export const SUPPORTED_FILE_TYPES = ['gif', 'jpeg', 'jpg', 'png', 'docx', 'gz', 'log', 'pdf', 'pptx', 'txt', 'xlsx', 'zip']
-                                    .concat(SUPPORTED_VIDEO_FILE_TYPES);
+export const SUPPORTED_FILE_TYPES = ['gif', 'jpeg', 'jpg', 'png', 'docx', 'gz', 'log', 'pdf', 'pptx', 'txt', 'xlsx', 'zip', ...SUPPORTED_VIDEO_FILE_TYPES];
 export const FILE_TYPE_SUPPORT_ERROR = 'We don\'t support that file type.' +
   ' Try again with ' + SUPPORTED_FILE_TYPES.join(', ') + '.';
 /**
