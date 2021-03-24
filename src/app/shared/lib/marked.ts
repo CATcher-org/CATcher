79a5c65af2c2d9cd70d7ef5b71ded1104d19a485
Sean Low <sean.low.jk@gmail.com>
2021-03-25 00:48:21 +0800

    Simplify Rerendering

--- src/app/shared/lib/marked.ts
+++ src/app/shared/lib/marked.ts
@@ -4,22 +4,10 @@ export function markedOptionsFactory(): MarkedOptions {
   const renderer = new MarkedRenderer();
   const linkRenderer = renderer.link;
 
-  function checkRegex(href): boolean {
-    const regex = /^(?:http:\/\/|https:\/\/)|(?!-)([A-Za-z0-9-]+(\.|-))+([A-Za-z]+)(?!-)$/g;
-    return regex.test(href);
-  }
 
   renderer.link = (href, title, text) => {
-    if (checkRegex(href)) {
-      if (!href.includes('http://') && !href.includes('https://')) {
-        href = `https://${href}`;
-      }
-      return linkRenderer.call(renderer, href, title, text);
-    } else {
-      const html = linkRenderer.call(renderer, href, title, text);
-      console.log(html);
-      return html.replace(/^<a /, '<a class="invalid-link ');
-    }
+    const html = linkRenderer.call(renderer, href, title, text);
+    return html.replace(/^<a /, '<a target="_blank" ');
   };
 
   return {
