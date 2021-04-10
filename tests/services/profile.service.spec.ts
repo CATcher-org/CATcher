e57c78e528deeb15cb0e06fa065d89c07d285817
Sean Low <sean.low.jk@gmail.com>
2021-04-10 22:16:27 +0800

    Revamp Mocking of Promises

--- tests/services/profile.service.spec.ts
+++ tests/services/profile.service.spec.ts
@@ -16,29 +16,6 @@ const INVALID_PROFILES = [
   }
 ];
 
-/**
- * Stub to mock the fetch API Call which retrieves the profiles
- * from the external repository.
- */
-class PromiseStub extends Promise<Response> {
-  readonly profiles: Object[];
-  constructor(profiles: Object[]) {
-    super(null);
-    this.profiles = profiles;
-  }
-
-  /**
-   * Overridden function to pass on the profiles as a Promise for the next step
-   * in fetchExternalProfiles
-   * @returns the json object with the given profiles.
-   */
-  then(onfulfilled?: (value: Response) => any, onrejected?: (reason: any) => PromiseLike<never>): Promise<any> {
-    return Promise.resolve({
-      profiles: this.profiles
-    });
-  }
-}
-
 describe('ProfileService', () => {
   beforeEach(() => {
     profileService = new ProfileService();
@@ -46,18 +23,23 @@ describe('ProfileService', () => {
 
   describe('.fetchExternalProfiles()', () => {
     it('should return the set of profiles if no profiles are given', () => {
-      spyOn(window, 'fetch').and.returnValue(new PromiseStub(EMPTY_PROFILES));
+      spyOn(window, 'fetch').and.returnValue(generateProfilesPromise(EMPTY_PROFILES));
       profileService.fetchExternalProfiles().then((profiles) => expect(profiles).toEqual(EMPTY_PROFILES));
     });
 
     it('should return the set of profiles if a set of valid profiles are given', () => {
-      spyOn(window, 'fetch').and.returnValue(new PromiseStub(VALID_PROFILES));
+      spyOn(window, 'fetch').and.returnValue(generateProfilesPromise(VALID_PROFILES));
       profileService.fetchExternalProfiles().then((profiles) => expect(profiles).toEqual(VALID_PROFILES));
     });
 
     it('should return the set of profiles if a set of valid profiles are given', () => {
-      spyOn(window, 'fetch').and.returnValue(new PromiseStub(INVALID_PROFILES));
+      spyOn(window, 'fetch').and.returnValue(generateProfilesPromise(INVALID_PROFILES));
       profileService.fetchExternalProfiles().catch((error: Error) => expect(error.message).toEqual('profiles.json is malformed'));
     });
   });
 });
+
+function generateProfilesPromise(profiles: any[]): Promise<Response> {
+  const mockResponse: Response = { json: () => Promise.resolve({ profiles: profiles }) } as Response;
+  return Promise.resolve(mockResponse);
+}
