f154408df75783079abc8e89e6cf76fa1bbd6feb
Sean Low <sean.low.jk@gmail.com>
2021-05-26 14:33:30 +0800

    Add Tests for Custom Renderer

--- src/app/shared/lib/marked.ts
+++ src/app/shared/lib/marked.ts
@@ -4,7 +4,6 @@ export function markedOptionsFactory(): MarkedOptions {
   const renderer = new MarkedRenderer();
   const linkRenderer = renderer.link;
 
-
   renderer.link = (href, title, text) => {
     const html = linkRenderer.call(renderer, href, title, text);
     return html.replace(/^<a /, '<a target="_blank" ');
