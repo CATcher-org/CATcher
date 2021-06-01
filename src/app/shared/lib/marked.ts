a685122b7b3515a3f105cead2e9274498d723ba8
Low Jun Kai, Sean <42912708+seanlowjk@users.noreply.github.com>
2021-06-01 12:36:12 +0800

    Markdown Links: Fix redirection href values  (#666)
    Markdown links: Fix redirection href values
    
    Our application crashes when an invalid link is rendered as HTML 
    and the user clicks on the HTML Link. This is because there are no
    checks done before the link is rendered. 
    
    Let's open these links in a new tabs and propagate these errors there 
    such that current users do not lose their current progress with respect 
    to bug reports and other form data.

---
+++ src/app/shared/lib/marked.ts
@@ -0,0 +1,22 @@
+import { MarkedOptions, MarkedRenderer } from "ngx-markdown";
+
+export function markedOptionsFactory(): MarkedOptions {
+  const renderer = new MarkedRenderer();
+  const linkRenderer = renderer.link;
+
+  renderer.link = (href, title, text) => {
+    const html = linkRenderer.call(renderer, href, title, text);
+    return html.replace(/^<a /, '<a target="_blank" ');
+  };
+
+  return {
+    renderer,
+    gfm: true,
+    tables: true,
+    breaks: false,
+    pedantic: false,
+    sanitize: false,
+    smartLists: true,
+    smartypants: false,
+  };
+}
