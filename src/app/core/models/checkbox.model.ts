f79951a0613854d39fee208df6079b6d8afa5ed0
RuiFeng <ruifeng.chang98@hotmail.com>
2021-08-19 20:57:49 +0800

    create checkbox model

---
+++ src/app/core/models/checkbox.model.ts
@@ -0,0 +1,19 @@
+export class Checkbox {
+	checkboxString: string; // in the format of - [ ] or - [x]
+
+  constructor(checkboxString: string){
+    this.checkboxString = checkboxString;
+  }
+
+  isChecked(): boolean {
+    return this.checkboxString.charAt(3) === 'x';
+  }
+
+  setChecked(isChecked: boolean) {
+    if (isChecked) {
+      this.checkboxString = this.checkboxString.replace('[ ]', '[x]');
+    } else {
+      this.checkboxString = this.checkboxString.replace('[x]', '[ ]');
+    }
+  }
+}
