b5ee4a3d12c497cf18cb989db3cddc0d442c12a2
chunweii <47494777+chunweii@users.noreply.github.com>
2022-02-07 11:24:28 +0800

    Abstract toaster component

---
+++ src/app/shared/error-toasters/toaster/toaster.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBarRef } from '@angular/material';
+
+@Component({
+  selector: 'app-toaster',
+  templateUrl: './toaster.component.html',
+  styleUrls: ['./toaster.component.css']
+})
+export class ToasterComponent implements OnInit {
+  constructor(public snackBarRef: MatSnackBarRef<ToasterComponent>) {}
+
+  ngOnInit() {}
+}
