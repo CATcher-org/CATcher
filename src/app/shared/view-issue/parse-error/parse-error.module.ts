646738fdecc7619b4c5b454e8fd9fdd457a0c693
Isaac Lee <68138671+luminousleek@users.noreply.github.com>
2022-02-28 18:52:30 +0800

    clean up code a bit

--- src/app/shared/view-issue/parse-error/parse-error.module.ts
+++ src/app/shared/view-issue/parse-error/parse-error.module.ts
@@ -1,11 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { ParseErrorComponent } from './parse-error.component';
 
 @NgModule({
   exports: [ParseErrorComponent],
   declarations: [ParseErrorComponent],
-  imports: [CommonModule, RouterModule]
+  imports: [CommonModule]
 })
 export class ParseErrorModule {}
