187aacc79ae9c892f43e3d3a7664ca60e47d8e59
Isaac Lee <68138671+luminousleek@users.noreply.github.com>
2022-03-07 18:58:09 +0800

    switch back to sequenceof

--- src/app/core/models/templates/team-response-template.model.ts
+++ src/app/core/models/templates/team-response-template.model.ts
@@ -4,7 +4,7 @@ import { DuplicateOfSection } from './sections/duplicate-of-section.model';
 import { Section } from './sections/section.model';
 import { Header, Template } from './template.model';
 
-const { choice, coroutine, digits, everyCharUntil, str, whitespace } = require('arcsecond');
+const { everyCharUntil, str, sequenceOf, whitespace } = require('arcsecond');
 
 export const TeamResponseHeaders = {
   teamResponse: new Header("Team's Response", 1),
@@ -14,34 +14,12 @@ export const TeamResponseHeaders = {
 const TEAM_RESPONSE_HEADER = "# Team's Response";
 const DUPLICATE_OF_HEADER = '## Duplicate status (if any):';
 
-const DuplicateNumberParser = coroutine(function* () {
-  yield str('Duplicate of #'); // parse and ignore
-  const issueNumber = yield digits; // parse and store duplicate issue number
-  return parseInt(issueNumber, 10); // issueNumber is a string, radix added to pass linting
-});
-
-const TeamResponseParser = coroutine(function* () {
-  yield str(TEAM_RESPONSE_HEADER); // parse and ignore header
-  yield whitespace; // parse and ignore newline character
-  const teamResponse = yield everyCharUntil(str(DUPLICATE_OF_HEADER)); // parse and store team's response
-
-  yield str(DUPLICATE_OF_HEADER);
-  yield whitespace;
-  let issueNumber = yield choice([
-    // either parse duplicate issue number or '--' if no duplicates
-    DuplicateNumberParser,
-    str('--')
-  ]);
-
-  if (issueNumber === '--') {
-    issueNumber = null;
-  }
-
-  return {
-    teamResponse: teamResponse.trim(), // remove trailing whitespace
-    issueNumber: issueNumber
-  };
-});
+const TeamResponseParser = sequenceOf([
+  str(TEAM_RESPONSE_HEADER),
+  whitespace,
+  everyCharUntil(str(DUPLICATE_OF_HEADER)),
+  str(DUPLICATE_OF_HEADER)
+]);
 
 export class TeamResponseTemplate extends Template {
   teamResponse: Section;
