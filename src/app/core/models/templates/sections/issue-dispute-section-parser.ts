28c1a2d3ba34119871cdf4797cf2472ae88effd1
Isaac Lee <68138671+luminousleek@users.noreply.github.com>
2022-03-15 21:02:24 +0800

    write issue dispute section parser

---
+++ src/app/core/models/templates/sections/issue-dispute-section-parser.ts
@@ -0,0 +1,17 @@
+const { coroutine, everyCharUntil, str, whitespace } = require('arcsecond');
+
+const SECTION_TITLE_PREFIX = '## :question: ';
+const TEAM_SAYS_HEADER = '### Team says:';
+const LINE_SEPARATOR = '-------------------';
+
+export const IssueDisputeSectionParser = coroutine(function* () {
+  yield str(SECTION_TITLE_PREFIX);
+  const title = yield everyCharUntil(str(TEAM_SAYS_HEADER));
+  yield whitespace;
+
+  const description = yield everyCharUntil(str(LINE_SEPARATOR));
+  return {
+    title: title.trim(),
+    description: description.trim()
+  };
+});
