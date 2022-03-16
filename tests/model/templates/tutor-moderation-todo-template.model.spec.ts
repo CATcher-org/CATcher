4dc892a0231b778a21994708beed9355d85d932d
Isaac Lee <68138671+luminousleek@users.noreply.github.com>
2022-03-16 11:41:39 +0800

    add test for tutor moderation todo parser

---
+++ tests/model/templates/tutor-moderation-todo-template.model.spec.ts
@@ -0,0 +1,40 @@
+import {
+  TutorModerationTodoParser,
+  TutorModerationTodoTemplate
+} from '../../../src/app/core/models/templates/tutor-moderation-todo-template.model';
+
+import { PENDING_TUTOR_MODERATION } from '../../constants/githubcomment.constants';
+
+const EMPTY_DONE_CHECKBOX = '- [ ] Done';
+const FILLED_DONE_CHECKBOX = '- [x] Done';
+const DEFAULT_TUTOR_RESPONSE = '[replace this with your explanation]';
+const TEST_TUTOR_RESPONSE = 'test';
+const DEFAULT_DESCRIPTION = EMPTY_DONE_CHECKBOX + '\n\n' + DEFAULT_TUTOR_RESPONSE;
+const TEST_DESCRIPTION = FILLED_DONE_CHECKBOX + '\n\n' + TEST_TUTOR_RESPONSE;
+
+const TYPE_TITLE = 'Issue Type';
+const SEVERITY_TITLE = 'Issue Severity';
+const NOT_RELATED_TITLE = 'Not Related Question';
+
+fdescribe('TutorModerationTodoParser', () => {
+  it('parses comment body correctly', () => {
+    const parsed = TutorModerationTodoParser.run(PENDING_TUTOR_MODERATION.body);
+    console.log(parsed);
+    const result = parsed.result;
+
+    expect(result[0].title).toBe(TYPE_TITLE);
+    expect(result[0].description).toBe(TEST_DESCRIPTION);
+    expect(result[0].todo.isChecked).toBe(true);
+    expect(result[0].tutorResponse).toBe(TEST_TUTOR_RESPONSE);
+
+    expect(result[1].title).toBe(SEVERITY_TITLE);
+    expect(result[1].description).toBe(DEFAULT_DESCRIPTION);
+    expect(result[1].todo.isChecked).toBe(false);
+    expect(result[1].tutorResponse).toBe(DEFAULT_DESCRIPTION);
+
+    expect(result[2].title).toBe(NOT_RELATED_TITLE);
+    expect(result[2].description).toBe(DEFAULT_DESCRIPTION);
+    expect(result[2].todo.isChecked).toBe(false);
+    expect(result[2].tutorResponse).toBe(DEFAULT_DESCRIPTION);
+  });
+});
