7dd7369dfa14752a7adff6217a5fee10d26888bb
Isaac Lee <68138671+luminousleek@users.noreply.github.com>
2022-03-16 10:47:45 +0800

    fix bug in moderation section parser

--- src/app/core/models/templates/sections/moderation-section-parser.model.ts
+++ src/app/core/models/templates/sections/moderation-section-parser.model.ts
@@ -15,11 +15,11 @@ export const ModerationSectionParser = coroutine(function* () {
   yield str(SECTION_TITLE_PREFIX);
   const title = yield everyCharUntil(str('- [')); // every char until the done checkbox
 
-  const description = yield lookAhead(everyCharUntil(LINE_SEPARATOR));
+  const description = yield lookAhead(everyCharUntil(str(LINE_SEPARATOR)));
 
   const doneCheckboxValue = yield DoneCheckboxParser;
   yield whitespace;
-  const tutorResponse = yield everyCharUntil(LINE_SEPARATOR);
+  const tutorResponse = yield everyCharUntil(str(LINE_SEPARATOR));
   yield str(LINE_SEPARATOR);
   yield optionalWhitespace;
 
