7187f4b92ecb48884056b0e95bb569c7d221f894
Isaac Lee <68138671+luminousleek@users.noreply.github.com>
2022-04-19 13:33:19 +0800

    fix lint

--- tests/app/shared/issue-tables/search-filter.spec.ts
+++ tests/app/shared/issue-tables/search-filter.spec.ts
@@ -3,7 +3,7 @@ import { Team } from '../../../../src/app/core/models/team.model';
 import { IssueService } from '../../../../src/app/core/services/issue.service';
 import { TABLE_COLUMNS } from '../../../../src/app/shared/issue-tables/issue-tables-columns';
 import { applySearchFilter } from '../../../../src/app/shared/issue-tables/search-filter';
-import { USER_ANUBHAV, USER_SHUMING } from '../../../constants/data.constants';
+import { USER_SHUMING } from '../../../constants/data.constants';
 import {
   DUPLICATED_ISSUE_WITH_EMPTY_DESCRIPTION_HIGH_SEVERITY,
   ISSUE_WITH_ASSIGNEES,
