58f25ced37a4df527df48bb2c49997bcb38a6742
James Yeap <jamesyeap.ys@gmail.com>
2022-07-25 16:44:09 +0800

    Generate toolbar component

--- src/app/shared/comment-editor/comment-editor.module.ts
+++ src/app/shared/comment-editor/comment-editor.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { MarkdownModule } from 'ngx-markdown';
 import { SharedModule } from '../shared.module';
 import { CommentEditorComponent } from './comment-editor.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   imports: [SharedModule, MarkdownModule.forChild()],
-  declarations: [CommentEditorComponent],
+  declarations: [CommentEditorComponent, ToolbarComponent],
   exports: [CommentEditorComponent]
 })
 export class CommentEditorModule {}
