1db02b454898a2e2cfac15fc9b1b0d5af2ecea03
James Yeap <jamesyeap.ys@gmail.com>
2022-07-25 16:58:15 +0800

    Add font awesome dependency for icons

--- src/app/shared/comment-editor/comment-editor.module.ts
+++ src/app/shared/comment-editor/comment-editor.module.ts
@@ -3,9 +3,10 @@ import { MarkdownModule } from 'ngx-markdown';
 import { SharedModule } from '../shared.module';
 import { CommentEditorComponent } from './comment-editor.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  imports: [SharedModule, MarkdownModule.forChild()],
+  imports: [SharedModule, MarkdownModule.forChild(), FontAwesomeModule],
   declarations: [CommentEditorComponent, ToolbarComponent],
   exports: [CommentEditorComponent]
 })
