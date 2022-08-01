eb035c856f8524abeb2422c2556a760be9e07595
James Yeap <jamesyeap.ys@gmail.com>
2022-08-01 14:33:36 +0800

    Generate toolbar component

--- src/app/shared/comment-editor/comment-editor.module.ts
+++ src/app/shared/comment-editor/comment-editor.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { MarkdownModule } from 'ngx-markdown';
 import { SharedModule } from '../shared.module';
 import { CommentEditorComponent } from './comment-editor.component';
+import { MarkdownToolbarComponent } from './markdown-toolbar/markdown-toolbar.component';
 
 @NgModule({
   imports: [SharedModule, MarkdownModule.forChild()],
-  declarations: [CommentEditorComponent],
+  declarations: [CommentEditorComponent, MarkdownToolbarComponent],
   exports: [CommentEditorComponent]
 })
 export class CommentEditorModule {}
---
+++ src/app/shared/comment-editor/markdown-toolbar/markdown-toolbar.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-markdown-toolbar',
+  templateUrl: './markdown-toolbar.component.html',
+  styleUrls: ['./markdown-toolbar.component.css']
+})
+export class MarkdownToolbarComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
