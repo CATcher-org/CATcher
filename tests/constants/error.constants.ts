d729b414fcd887099f92cd2629d4350cbcbb5114
Cedric Ong <cedricong99@gmail.com>
2022-08-21 11:04:04 +0800

    Add testing for 401 and 404 errors

--- tests/constants/error.constants.ts
+++ tests/constants/error.constants.ts
@@ -3,8 +3,6 @@ import { RequestError } from '@octokit/request-error';
 
 export const STANDARD_ERROR = new Error('This is a normal error');
 
-export const ERROR_WITH_NODE_MODULES = new Error(`node_modules: there was a problem\n There was an error`);
-
 export const ERROR_WITH_NO_MESSAGE = new Error();
 
 export const HTTP_304_ERROR = new HttpErrorResponse({ status: 304 });
@@ -14,6 +12,10 @@ export const HTTP_422_ERROR = new HttpErrorResponse({ status: 422 });
 export const HTTP_500_ERROR = new HttpErrorResponse({ status: 500 });
 
 export const HTTP_400_ERROR = new HttpErrorResponse({ status: 400 });
+
+export const HTTP_401_ERROR = new HttpErrorResponse({ status: 401 });
+
+export const HTTP_404_ERROR = new HttpErrorResponse({ status: 404 });
 
 export const OCTOKIT_REQUEST_ERROR = new RequestError('This is an octokit request error', 400, {
   request: { method: 'GET', url: '', headers: {} }
--- tests/services/error-handling.service.spec.ts
+++ tests/services/error-handling.service.spec.ts
@@ -3,9 +3,10 @@ import { FormErrorComponent } from '../../src/app/shared/error-toasters/form-err
 import { GeneralMessageErrorComponent } from '../../src/app/shared/error-toasters/general-message-error/general-message-error.component';
 import {
   ERROR_WITH_NO_MESSAGE,
-  ERROR_WITH_NODE_MODULES,
   HTTP_304_ERROR,
   HTTP_400_ERROR,
+  HTTP_401_ERROR,
+  HTTP_404_ERROR,
   HTTP_422_ERROR,
   HTTP_500_ERROR,
   OCTOKIT_REQUEST_ERROR,
@@ -23,45 +24,51 @@ describe('ErrorHandlingService', () => {
     errorHandlingService = new ErrorHandlingService(mockSnackBar, mockLoggingService);
   });
 
-  it('should log errors when handling errors', () => {
-    errorHandlingService.handleError(STANDARD_ERROR);
-    expect(mockLoggingService.error).toHaveBeenCalledWith(STANDARD_ERROR);
-  });
-
-  it('should use the GeneralMessageErrorComponent when handling Errors', () => {
-    errorHandlingService.handleError(STANDARD_ERROR);
-    expect(mockSnackBar.openFromComponent).toHaveBeenCalledWith(GeneralMessageErrorComponent, {
-      data: { message: STANDARD_ERROR.message }
+  describe('ErrorHandlingService: handleError()', () => {
+    it('should log errors when handling errors', () => {
+      errorHandlingService.handleError(STANDARD_ERROR);
+      expect(mockLoggingService.error).toHaveBeenCalledWith(STANDARD_ERROR);
     });
-  });
 
-  it('should stringify Errors if there is no message before displaying', () => {
-    errorHandlingService.handleError(ERROR_WITH_NO_MESSAGE);
-    expect(mockSnackBar.openFromComponent).toHaveBeenCalledWith(GeneralMessageErrorComponent, {
-      data: { message: JSON.stringify(STANDARD_ERROR) }
+    it('should use the GeneralMessageErrorComponent when handling Errors', () => {
+      errorHandlingService.handleError(STANDARD_ERROR);
+      expect(mockSnackBar.openFromComponent).toHaveBeenCalledWith(GeneralMessageErrorComponent, {
+        data: { message: STANDARD_ERROR.message }
+      });
     });
-  });
 
-  it('should not open the snackbar when handling http status 304 errors', () => {
-    errorHandlingService.handleError(HTTP_304_ERROR);
-    expect(mockSnackBar.openFromComponent).not.toHaveBeenCalled();
-  });
+    it('should stringify Errors if there is no message before displaying', () => {
+      errorHandlingService.handleError(ERROR_WITH_NO_MESSAGE);
+      expect(mockSnackBar.openFromComponent).toHaveBeenCalledWith(GeneralMessageErrorComponent, {
+        data: { message: JSON.stringify(ERROR_WITH_NO_MESSAGE) }
+      });
+    });
 
-  it('should use the FormErrorComponent when handling http status 422 errors', () => {
-    errorHandlingService.handleError(HTTP_422_ERROR);
-    expect(mockSnackBar.openFromComponent).toHaveBeenCalledWith(FormErrorComponent, { data: HTTP_422_ERROR });
-  });
+    it('should not open the snackbar when handling http status 304 errors', () => {
+      errorHandlingService.handleError(HTTP_304_ERROR);
+      expect(mockSnackBar.openFromComponent).not.toHaveBeenCalled();
+    });
 
-  it('should use the GeneralMessageErrorComponent when handling other http errors', () => {
-    errorHandlingService.handleError(HTTP_500_ERROR);
-    expect(mockSnackBar.openFromComponent).toHaveBeenCalledWith(GeneralMessageErrorComponent, { data: HTTP_500_ERROR });
-    errorHandlingService.handleError(HTTP_400_ERROR);
-    expect(mockSnackBar.openFromComponent).toHaveBeenCalledWith(GeneralMessageErrorComponent, { data: HTTP_400_ERROR });
-    expect(mockSnackBar.openFromComponent).toHaveBeenCalledTimes(2);
-  });
+    it('should use the FormErrorComponent when handling http status 422 errors', () => {
+      errorHandlingService.handleError(HTTP_422_ERROR);
+      expect(mockSnackBar.openFromComponent).toHaveBeenCalledWith(FormErrorComponent, { data: HTTP_422_ERROR });
+    });
 
-  it('should treat octokit request errors as http errors', () => {
-    errorHandlingService.handleError(OCTOKIT_REQUEST_ERROR);
-    expect(mockSnackBar.openFromComponent).toHaveBeenCalledWith(GeneralMessageErrorComponent, { data: OCTOKIT_REQUEST_ERROR });
+    it('should use the GeneralMessageErrorComponent when handling other http errors', () => {
+      errorHandlingService.handleError(HTTP_500_ERROR);
+      expect(mockSnackBar.openFromComponent).toHaveBeenCalledWith(GeneralMessageErrorComponent, { data: HTTP_500_ERROR });
+      errorHandlingService.handleError(HTTP_400_ERROR);
+      expect(mockSnackBar.openFromComponent).toHaveBeenCalledWith(GeneralMessageErrorComponent, { data: HTTP_400_ERROR });
+      errorHandlingService.handleError(HTTP_401_ERROR);
+      expect(mockSnackBar.openFromComponent).toHaveBeenCalledWith(GeneralMessageErrorComponent, { data: HTTP_401_ERROR });
+      errorHandlingService.handleError(HTTP_404_ERROR);
+      expect(mockSnackBar.openFromComponent).toHaveBeenCalledWith(GeneralMessageErrorComponent, { data: HTTP_404_ERROR });
+      expect(mockSnackBar.openFromComponent).toHaveBeenCalledTimes(4);
+    });
+
+    it('should treat octokit request errors as http errors', () => {
+      errorHandlingService.handleError(OCTOKIT_REQUEST_ERROR);
+      expect(mockSnackBar.openFromComponent).toHaveBeenCalledWith(GeneralMessageErrorComponent, { data: OCTOKIT_REQUEST_ERROR });
+    });
   });
 });
