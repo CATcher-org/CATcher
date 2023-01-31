4adb9b52566ba93002c4cbd3fa3cea8f8dc8a35b
Lee Chun Wei <47494777+chunweii@users.noreply.github.com>
2023-01-31 11:20:33 +0800

    Merge V3.4.4 (#1026)
    Merge release branch to master

--- src/app/core/models/templates/tester-response-template.model.ts
+++ src/app/core/models/templates/tester-response-template.model.ts
@@ -5,7 +5,7 @@ import { buildTeamResponseSectionParser } from './section-parsers/common-parsers
 import { TesterResponseSectionParser } from './section-parsers/tester-response-section-parser.model';
 import { Template } from './template.model';
 
-const { coroutine, many1, str, whitespace } = require('arcsecond');
+const { coroutine, many1, str, optionalWhitespace, possibly, whitespace } = require('arcsecond');
 
 interface TesterResponseParseResult {
   teamResponse: string;
@@ -15,12 +15,17 @@ interface TesterResponseParseResult {
   teamChosenType: string;
 }
 
+const GITHUB_UI_EDIT_WARNING =
+  '[IMPORTANT!: Please do not edit or reply to this comment using the GitHub UI. You can respond to it using CATcher during the next phase of the PE]';
 const TESTER_RESPONSES_HEADER = '# Items for the Tester to Verify';
 const DISAGREE_CHECKBOX_DESCRIPTION = 'I disagree';
 
 const TeamResponseSectionParser = buildTeamResponseSectionParser(TESTER_RESPONSES_HEADER);
 
 export const TesterResponseParser = coroutine(function* () {
+  yield possibly(str(GITHUB_UI_EDIT_WARNING));
+  yield optionalWhitespace;
+
   const teamResponse = yield TeamResponseSectionParser;
 
   // parse tester responses from comment
