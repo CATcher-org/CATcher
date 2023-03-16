827e17dc6d99706e2c99204f88d2a5e73fb8d27e
Vignesh Sankar Iyer <68676914+vigneshsankariyer1234567890@users.noreply.github.com>
2023-03-16 14:01:23 +0800

    Remove electron dependencies  (#1153)
    Refactor and remove uses of Electron from CATcher
    
    There are many uses of Electron in CATcher, even with the CATcher Electron
    app being deprecated.  As such, there are many instances of unused code,
    which is bad practice. Removing instances of uses of Electron allows us to
    trim the codebase and remove dead code.
    
    Let's 
    * Refactor and remove uses of Electron from CATcher
    * Remove all uses of Electron in the `src` folder first.
    * Remove uses of Electron from `package.json` and other files
    * Delete folders such as `electron-utils` and files such as `postinstall` that
    are used to initialise electron
    * Remove and amend scripts in `package.json` that are involved in
    building the electron app
    * Edit `deployment-actions.yml` to remove pre-build actions from CD
    workflow

--- src/app/core/services/factories/factory.github.service.ts
+++ src/app/core/services/factories/factory.github.service.ts
@@ -1,14 +1,13 @@
 import { Apollo } from 'apollo-angular';
 import { AppConfig } from '../../../../environments/environment';
-import { ElectronService } from '../electron.service';
 import { ErrorHandlingService } from '../error-handling.service';
 import { GithubService } from '../github.service';
 import { LoggingService } from '../logging.service';
 import { MockGithubService } from '../mocks/mock.github.service';
 
-export function GithubServiceFactory(handling: ErrorHandlingService, apollo: Apollo, electron: ElectronService, logger: LoggingService) {
+export function GithubServiceFactory(handling: ErrorHandlingService, apollo: Apollo, logger: LoggingService) {
   if (AppConfig.test) {
     return new MockGithubService();
   }
-  return new GithubService(handling, apollo, electron, logger);
+  return new GithubService(handling, apollo, logger);
 }
