c8b68de8a5f9b04e85a751b9c7b05d95ced10d81
ZQ <e0725180@u.nus.edu>
2023-03-21 13:05:05 +0800

    Removes unused import

--- src/app/shared/issue/description/description.component.ts
+++ src/app/shared/issue/description/description.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, HostBinding, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, NgForm } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { throwError } from 'rxjs';
