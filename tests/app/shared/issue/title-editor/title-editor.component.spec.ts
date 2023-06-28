b6f4b04b9d64aabe2290ea7ba73e5d789a2746a0
chia-yh <0.chiayuhong.0@gmail.com>
2023-06-28 14:45:08 +0800

    Add additional tests for title-editor.component

--- tests/app/shared/issue/title-editor/title-editor.component.spec.ts
+++ tests/app/shared/issue/title-editor/title-editor.component.spec.ts
@@ -7,12 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TitleEditorComponent } from '../../../../../src/app/shared/issue/title-editor/title-editor.component';
 import { SharedModule } from '../../../../../src/app/shared/shared.module';
 
-describe('CommentEditor', () => {
+describe('TitleEditor', () => {
   let fixture: ComponentFixture<TitleEditorComponent>;
   let debugElement: DebugElement;
   let component: TitleEditorComponent;
 
   const TEST_INITIAL_TITLE = 'abc';
+  const TEST_257_CHARS = '0'.repeat(257);
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -68,8 +69,73 @@ describe('CommentEditor', () => {
       textBox.value = '123';
       textBox.dispatchEvent(new Event('input'));
 
+      await fixture.whenStable().then(() => {
+        expect(textBox.value).toEqual('123');
+      });
+    });
+
+    it('should undo a change on undo input', () => {
+      fixture.detectChanges();
+
+      const textBox: any = debugElement.query(By.css('input')).nativeElement;
+
+      // saves empty state: { text: '', selectStart: 0, selectEnd: 0 }
+      component.history.forceSave();
+
+      textBox.value = '123';
+      // saves state: { text: '123', selectStart: 3, selectEnd: 3 }
+      component.history.forceSave();
+
+      // undo to empty state: { text: '', selectStart: 0, selectEnd: 0 }
+      textBox.dispatchEvent(new KeyboardEvent('keydown', {key: 'z', code: 'KeyZ', ctrlKey: true}));
+
+      fixture.whenStable().then(() => {
+        expect(textBox.value).toEqual('');
+      });
+    });
+
+    it('should redo an undone change on redo input', () => {
+      fixture.detectChanges();
+
+      const textBox: any = debugElement.query(By.css('input')).nativeElement;
+
+      // saves empty state: { text: '', selectStart: 0, selectEnd: 0 }
+      component.history.forceSave();
+
+      textBox.value = '123';
+      // saves state: { text: '123', selectStart: 3, selectEnd: 3 }
+      component.history.forceSave();
+
+      // undo to empty state: { text: '', selectStart: 0, selectEnd: 0 }
+      textBox.dispatchEvent(new KeyboardEvent('keydown', {key: 'z', code: 'KeyZ', ctrlKey: true}));
+
+      // redo to state: { text: '123', selectStart: 3, selectEnd: 3 }
+      textBox.dispatchEvent(new KeyboardEvent('keydown', {key: 'y', code: 'KeyY', ctrlKey: true}));
+
       fixture.whenStable().then(() => {
         expect(textBox.value).toEqual('123');
+      });
+    });
+
+    it('blank input should be invalid', () => {
+      fixture.autoDetectChanges();
+
+      fixture.whenStable().then(() => {
+        expect(component.titleField.valid).toBe(false);
+        expect(component.titleField.hasError('required')).toEqual(true);
+      });
+    });
+
+    it('input of more than 256 characters should be invalid', async () => {
+      fixture.detectChanges();
+
+      const textBox: any = debugElement.query(By.css('input')).nativeElement;
+      textBox.value = TEST_257_CHARS;
+      textBox.dispatchEvent(new Event('input'));
+
+      await fixture.whenStable().then(() => {
+        expect(component.titleField.valid).toBe(false);
+        expect(component.titleField.hasError('maxlength')).toEqual(true);
       });
     });
   });
