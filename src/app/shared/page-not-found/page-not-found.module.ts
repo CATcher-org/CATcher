f85bde050dff48ca9d560b0bea568c6d3c88e119
Nereus_Ng_Wei_Bin <iamnereusng@gmail.com>
2024-01-20 15:09:41 +0800

    Fix import statement order

--- src/app/shared/page-not-found/page-not-found.module.ts
+++ src/app/shared/page-not-found/page-not-found.module.ts
@@ -1,7 +1,7 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PageNotFoundComponent } from './page-not-found.component';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found.component';
 
 @NgModule({
   declarations: [PageNotFoundComponent],
