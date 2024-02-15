67c2c85bafefc471762ea3a498d5d93ae3cadf9f
Arif <arifkhalid99@gmail.com>
2024-02-15 20:09:53 +0800

    Document new service

--- src/app/core/services/issue-table-settings.service.ts
+++ src/app/core/services/issue-table-settings.service.ts
@@ -3,6 +3,11 @@ import { TableSettings } from '../models/table-settings.model';
 @Injectable({
   providedIn: 'root'
 })
+
+/**
+ * Responsible for storing and retrieving the table settings for issue tables created
+ * Map is required since there can be multiple tables within the same page
+ */
 export class IssueTableSettingsService {
   private _tableSettingsMap = {};
 
