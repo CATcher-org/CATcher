b276b0c49ec9fcc24f018d172b4b1501776a5981
Wong Chee Hong <c.h.wong2606@gmail.com>
2024-04-26 15:26:14 +0800

    Apply exceptions

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,9 +1,11 @@
 /* SystemJS module definition */
+// eslint-disable-next-line no-var -- Please revisit this eslint rule in the future
 declare var nodeModule: NodeModule;
 interface NodeModule {
   id: string;
 }
 
+// eslint-disable-next-line no-var -- Please revisit this eslint rule in the future
 declare var window: Window & typeof globalThis;
 interface Window {
   process: any;
