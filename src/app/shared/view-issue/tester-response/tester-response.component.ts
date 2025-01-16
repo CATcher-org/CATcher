d4481904359866fd10887e4abffe3dee7cac4144
wen xi <tngwenxi@gmail.com>
2025-01-16 14:49:15 +0800

    Move UI edit warning to createGithubTesterResponse()

--- src/app/shared/view-issue/tester-response/tester-response.component.ts
+++ src/app/shared/view-issue/tester-response/tester-response.component.ts
@@ -15,7 +15,6 @@ import { UserService } from '../../../core/services/user.service';
 import { CommentEditorComponent } from '../../comment-editor/comment-editor.component';
 import { SUBMIT_BUTTON_TEXT } from '../view-issue.component';
 import { ConflictDialogComponent, TesterResponseConflictData } from './conflict-dialog/conflict-dialog.component';
-import { GITHUB_UI_EDIT_WARNING } from '../../../core/models/templates/tester-response-template.model';
 
 @Component({
   selector: 'app-tester-response',
@@ -79,7 +78,7 @@ export class TesterResponseComponent implements OnInit, OnChanges {
           if (isSaveToSubmit || this.isUpdatingDeletedResponse() || this.submitButtonText === SUBMIT_BUTTON_TEXT.OVERWRITE) {
             return this.issueService.updateTesterResponse(this.issue, <IssueComment>{
               ...this.issue.issueComment,
-              description: GITHUB_UI_EDIT_WARNING + '\n' + this.getTesterResponseFromForm()
+              description: this.getTesterResponseFromForm()
             });
           } else {
             this.submitButtonText = SUBMIT_BUTTON_TEXT.OVERWRITE;
