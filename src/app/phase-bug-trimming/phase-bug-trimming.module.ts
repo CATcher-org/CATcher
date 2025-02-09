8b251777a4e2a1e524f1e177794309661fafb32d
wen xi <tngwenxi@gmail.com>
2025-02-09 15:13:47 +0800

    Create issue component and add routing

--- src/app/phase-bug-trimming/phase-bug-trimming.module.ts
+++ src/app/phase-bug-trimming/phase-bug-trimming.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { PhaseBugTrimmingRoutingModule } from './phase-bug-trimming-routing.module';
 import { PhaseBugTrimmingComponent } from './phase-bug-trimming.component';
+import { IssueComponent } from './issue/issue.component';
 
 @NgModule({
   imports: [PhaseBugTrimmingRoutingModule, SharedModule],
-  declarations: [PhaseBugTrimmingComponent]
+  declarations: [PhaseBugTrimmingComponent, IssueComponent]
 })
 export class PhaseBugTrimmingModule {}
