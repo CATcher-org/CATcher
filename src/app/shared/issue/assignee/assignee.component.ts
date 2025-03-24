f19462deb7abb4879dd6c3754e6cf5b8add5ab0b
yucongkoo <122191710+yucongkoo@users.noreply.github.com>
2025-03-24 16:49:10 +0800

    Fix bug related to assignees dropdown showing old values (#1349)
    Fix bug related to assignees dropdown showing old values

--- src/app/shared/issue/assignee/assignee.component.ts
+++ src/app/shared/issue/assignee/assignee.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, ViewChild, ViewEncapsulation } from '@angular/core';
 import { MatSelect } from '@angular/material/select';
 import { first } from 'rxjs/operators';
 import { Issue } from '../../../core/models/issue.model';
@@ -14,7 +14,7 @@ import { PhaseService } from '../../../core/services/phase.service';
   styleUrls: ['./assignee.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class AssigneeComponent implements OnInit {
+export class AssigneeComponent implements OnInit, OnChanges {
   teamMembers: string[];
   isInEditMode = false;
   assignees: string[];
@@ -37,6 +37,12 @@ export class AssigneeComponent implements OnInit {
   ngOnInit(): void {
     this.teamMembers = this.team.teamMembers.map((user) => user.loginId);
     this.assignees = this.issue.assignees.map((a) => a);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['issue']) {
+      this.assignees = [...(this.issue?.assignees || [])];
+    }
   }
 
   openSelector() {
